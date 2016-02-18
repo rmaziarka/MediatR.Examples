c77f5b4a14f0816c0d9e562050dfbbd192eb8f49
Patryk Kulecki <pkulecki@objectivity.co.uk>
2016-02-18 15:21:51 +0100

    #20187, #20301 - created simple UI test Rev. T.Bień

---
+++ src/KnightFrank.Antares.UI.Tests/ProjectTestBase.cs
@@ -0,0 +1,127 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Objectivity Bespoke Software Specialists
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace Objectivity.Test.Automation.Tests.Features
+{
+    using System;
+
+    using Xunit;
+    using Common;
+    using Common.Logger;
+
+    using TechTalk.SpecFlow;
+
+    /// <summary>
+    /// The base class for all tests
+    /// </summary>
+    [Binding]
+    public class ProjectTestBase : TestBase
+    {
+        private readonly ScenarioContext scenarioContext;
+        private readonly DriverContext driverContext = new DriverContext();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectTestBase"/> class.
+        /// </summary>
+        public ProjectTestBase(ScenarioContext scenarioContext)
+        {
+            if (scenarioContext == null) throw new ArgumentNullException("scenarioContext");
+            this.scenarioContext = scenarioContext;
+        }
+
+        /// <summary>
+        /// Logger instance for driver
+        /// </summary>
+        public TestLogger LogTest
+        {
+            get
+            {
+                return this.DriverContext.LogTest;
+            }
+
+            set
+            {
+                this.DriverContext.LogTest = value;
+            }
+        }
+
+        /// <summary>
+        /// The browser manager
+        /// </summary>
+        protected DriverContext DriverContext
+        {
+            get
+            {
+                return this.driverContext;
+            }
+        }
+
+        /// <summary>
+        /// Before the class.
+        /// </summary>
+        [BeforeFeature]
+        public static void BeforeClass()
+        {
+            StartPerformanceMeasure();
+        }
+
+        /// <summary>
+        /// After the class.
+        /// </summary>
+        [AfterFeature]
+        public static void AfterClass()
+        {
+            StopPerfromanceMeasure();
+        }
+
+        /// <summary>
+        /// Before the test.
+        /// </summary>
+        [Before]
+        public void BeforeTest()
+        {
+            this.DriverContext.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            this.DriverContext.TestTitle = this.scenarioContext.ScenarioInfo.Title;
+            this.LogTest.LogTestStarting(this.driverContext);
+            this.DriverContext.Start();
+            this.scenarioContext["DriverContext"] = this.DriverContext;
+        }
+
+        /// <summary>
+        /// After the test.
+        /// </summary>
+        [After]
+        public void AfterTest()
+        {
+            this.DriverContext.IsTestFailed = this.scenarioContext.TestError != null;
+            this.SaveTestDetailsIfTestFailed(this.driverContext);
+            this.DriverContext.Stop();
+            this.LogTest.LogTestEnding(this.driverContext);
+            if (this.IsVerifyFailedAndClearMessages(this.driverContext))
+            {
+                Assert.True(false);
+            }
+        }
+    }
+}
---
+++ src/KnightFrank.Antares.UI.Tests/Steps/GoogleSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using KnightFrank.Antares.UI.Tests.Pages;
+using Objectivity.Test.Automation.Common;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace KnightFrank.Antares.UI.Tests.Steps
+{
+    [Binding]
+    public class GoogleSteps
+    {
+        private readonly DriverContext driverContext;
+        private readonly ScenarioContext scenarioContext;
+
+        public GoogleSteps(ScenarioContext scenarioContext)
+        {
+            if (scenarioContext == null) throw new ArgumentNullException("scenarioContext");
+            this.scenarioContext = scenarioContext;
+
+            driverContext = this.scenarioContext["DriverContext"] as DriverContext;
+        }
+
+        [Given(@"User navigates to google page")]
+        public void OpenGooglePage()
+        {
+            var googlePage = new GooglePage(driverContext).OpenGooglePage();
+            ScenarioContext.Current["GooglePage"] = googlePage;
+        }
+
+        [When(@"User looks for '(.*)' in google")]
+        public void SetSearchCriteria(string criteria)
+        {
+            ScenarioContext.Current.Get<GooglePage>("GooglePage").SetSearchCriteria(criteria).ClickSearchButton();
+        }
+
+        [Then(@"results should be visible")]
+        public void CheckResults()
+        {
+            var googlePage = ScenarioContext.Current.Get<GooglePage>("GooglePage");
+            Assert.True(googlePage.CheckIfResultsAreDisplayed(), "Results are not displayed");
+        }
+    }
+}
