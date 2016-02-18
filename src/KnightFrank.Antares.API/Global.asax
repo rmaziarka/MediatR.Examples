9d5eaeabb73f5e9913b5dfa777e25795d59fe5f9
rseniuta <rseniuta@objectivity.co.uk>
2016-02-18 12:31:25 +0100

    #20183 #20274 Created solution with empty projects
    Reviewed by: Adam Lepkowski

---
+++ src/KnightFrank.Antares.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KnightFrank.Antares.Api.WebApiApplication" Language="C#" %>
---
+++ src/KnightFrank.Antares.API/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace KnightFrank.Antares.Api
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
