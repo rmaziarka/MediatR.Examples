d5a7030abf0732628e8ef44efd65ea961d91021f
Karolina Czerniawska <kczerniawska@PLKCZERNIAWSKA.objectivity.co.uk>
2016-02-19 15:41:59 +0100

    #19862 #20364 Added contact controller + added swagger
    Reviewed by: Julian Cmikiewicz

---
+++ src/KnightFrank.Antares.API/Controllers/ContactController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using KnightFrank.Antares.API.Models;
+
+namespace KnightFrank.Antares.API.Controllers
+{
+    /// <summary>
+    /// Controller class for contacts
+    /// </summary>
+    [RoutePrefix("api/contacts")]
+    public class ContactController : ApiController
+    {
+        /// <summary>
+        /// Get contact list
+        /// </summary>
+        /// <returns>Contact entity collection</returns>
+        [HttpGet]
+        public IEnumerable<ContactDto> GetContacts()
+        {
+            return new [] { new ContactDto() { FirstName = "value", Surname = "value" } };
+        }
+
+        /// <summary>
+        /// Get contact
+        /// </summary>
+        /// <param name="id">Contact id</param>
+        /// <returns>Contact entity</returns>
+        [HttpGet]
+        [Route("{id}")]
+        public ContactDto GetContact(int id)
+        {
+            return new ContactDto() {FirstName = "value", Surname = "value"};
+        }
+
+        /// <summary>
+        /// Create contact
+        /// </summary>
+        /// <param name="contact">Contact entity</param>
+        [HttpPost]
+        public void CreateContact([FromBody]ContactDto contact)
+        {
+        }
+
+        /// <summary>
+        /// Update contact
+        /// </summary>
+        /// <param name="id">Contact id</param>
+        /// <param name="contact">Contact entity</param>
+        [HttpPut]
+        [Route("{id}")]
+        public void UpdateContact(int id, [FromBody]ContactDto contact)
+        {
+        }
+
+        /// <summary>
+        /// Delete contact
+        /// </summary>
+        /// <param name="id">Contact id</param>
+        [HttpDelete]
+        [Route("{id}")]
+        public void DeleteContact(int id)
+        {
+        }
+    }
+}
---
+++ src/KnightFrank.Antares.API/Models/ContactDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnightFrank.Antares.API.Models
+{
+    /// <summary>
+    /// Contact DTO
+    /// </summary>
+    public class ContactDto
+    {
+        /// <summary>
+        /// First name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name
+        /// </summary>
+        public string Surname { get; set; }
+    }
+}
