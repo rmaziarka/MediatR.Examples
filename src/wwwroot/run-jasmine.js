2d524b428e4272b4752b62c31953e23a4af85c93
Marcin Biernacik <mbiernacik@objectivity.co.uk>
2016-02-24 14:46:07 +0100

    #20185 #20585 Define the CI Build Process and Build Steps. Run javascript tests. Modified run-jasmine.js file to work with Jasmine 2.4.x. Added correct version of phantomjs.exe
    Reviewed by: Kamil Haladus

--- src/wwwroot/run-jasmine.js
+++ src/wwwroot/run-jasmine.js
@@ -102,32 +102,34 @@ page.open(system.args[1], function (status) {
     } else {
         waitFor(function () {
             return page.evaluate(function () {
-                return document.body.querySelector('.symbolSummary .pending') === null
+                return document.body.querySelector('.jasmine-symbolSummary .jasmine-pending') === null &&
+                    (document.body.querySelector('.jasmine-alert > .jasmine-bar.jasmine-passed') !== null ||
+                     document.body.querySelector('.jasmine-alert > .jasmine-bar.jasmine-failed') !== null);
             });
         }, function () {
             var exitCode = page.evaluate(function () {
                 var currentSuite;
-                var successList = document.body.querySelectorAll('.results > .summary .specs > .passed');
+                var successList = document.body.querySelectorAll('.jasmine-results > .jasmine-summary .jasmine-specs > .jasmine-passed');
                 var suites = {};
                 if (successList && successList.length > 0) {
                     for (var i = 0; i < successList.length; ++i) {
                         var el = successList[i],
                             name = el.children[0].innerText,
-                            suite = el.parentElement.parentElement.querySelector('.suite-detail').innerText;
+                            suite = el.parentElement.parentElement.querySelector('.jasmine-suite-detail').innerText;
 
                         suites[suite] = suites[suite] || [];
                         suites[suite].push({ status: 'success', name: name });
                     }
                 }
 
-                var failedList = document.body.querySelectorAll('.results > .failures > .spec-detail.failed');
+                var failedList = document.body.querySelectorAll('.jasmine-results > .jasmine-failures > .jasmine-spec-detail.jasmine-failed');
                 if (failedList && failedList.length > 0) {
                     console.log('');
                     console.log(failedList.length + ' test(s) FAILED:');
                     for (var i = 0; i < failedList.length; ++i) {
                         var el = failedList[i],
-                            name = el.querySelector('.description').innerText,
-                            msg = el.querySelector('.result-message').innerText,
+                            name = el.querySelector('.jasmine-description').innerText,
+                            msg = el.querySelector('.jasmine-result-message').innerText,
                             suite = name.substring(0, name.indexOf(' '));
 
                         // remove trailing period
@@ -162,7 +164,7 @@ page.open(system.args[1], function (status) {
                 if (failedList && failedList.length > 0) {
                     return 1;
                 } else {
-                    console.log(document.body.querySelector('.alert > .bar.passed').innerText);
+                    console.log(document.body.querySelector('.jasmine-alert > .jasmine-bar.jasmine-passed').innerText);
                     return 0;
                 }
             });
