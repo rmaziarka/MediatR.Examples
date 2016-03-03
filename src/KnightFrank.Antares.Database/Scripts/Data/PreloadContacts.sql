8b2c47f41b9e884853662865250766f6a5264aa2
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-03-03 16:14:11 +0100

    #20401 #21054 SSDT project Reviewed by: Marta Dycjan

---
+++ src/KnightFrank.Antares.Database/Scripts/Data/PreloadContacts.sql
@@ -0,0 +1,20 @@
+﻿MERGE dbo.Contacts c
+USING (VALUES
+	('John', 'Smith', 'test 1'),
+	('Alan', 'Rolfe', 'test 2')
+) as d(FirstName, Surname, Title)
+ON c.FirstName = d.FirstName AND c.Surname = d.Surname
+WHEN MATCHED THEN
+	UPDATE SET		
+		c.Title = d.Title
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT(		
+		FirstName, 
+		Surname, 
+		Title
+	) VALUES (		
+		d.FirstName, 
+		d.Surname, 
+		d.Title
+	);
+GO
