8124f1caaab6d769f53514439095c277127a5290
Marcin Biernacik <mbiernacik@objectivity.co.uk>
2016-03-07 14:18:27 +0100

    #19862 #20363 Create Contact with Basic Details. Create business validations. Fixed length rule.
    Reviewed by: Ryszard Seniuta

--- src/KnightFrank.Antares.Domain/Contact/Commands/UpdateContactCommandValidator.cs
+++ src/KnightFrank.Antares.Domain/Contact/Commands/UpdateContactCommandValidator.cs
@@ -7,9 +7,9 @@
         public UpdateContactCommandValidator()
         {
             this.RuleFor(x => x.Id).NotEmpty();
-            this.RuleFor(x => x.FirstName).NotEmpty().Length(255);
-            this.RuleFor(x => x.Surname).NotEmpty().Length(255);
-            this.RuleFor(x => x.Title).NotEmpty().Length(255);
+            this.RuleFor(x => x.FirstName).NotEmpty().Length(1, 255);
+            this.RuleFor(x => x.Surname).NotEmpty().Length(1, 255);
+            this.RuleFor(x => x.Title).NotEmpty().Length(1, 255);
         }
     }
 }
