c1dfa83bd2cd5da9446ab0e0cb12723a5d494543
Marcin Biernacik <mbiernacik@objectivity.co.uk>
2016-03-09 08:39:15 +0100

    #21192 #21195 Dictionary database structure. Create enum controller for retrieving enums (support localisation) Added seed method to populate tables with data.
    Reviewed by: Karolina Czerniawska

--- src/KnightFrank.Antares.Dal/Migrations/Configuration.cs
+++ src/KnightFrank.Antares.Dal/Migrations/Configuration.cs
@@ -2,11 +2,20 @@ namespace KnightFrank.Antares.Dal.Migrations
 {
     using System.Data.Entity.Migrations;
 
+    using KnightFrank.Antares.Dal.Seed;
+
     internal sealed class Configuration : DbMigrationsConfiguration<KnightFrankContext>
     {
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(KnightFrankContext context)
+        {
+            EntityTypeEnum.Seed(context);
+
+            context.SaveChanges();
         }
     }
 }
---
+++ src/KnightFrank.Antares.Dal/Seed/EntityTypeEnum.cs
@@ -0,0 +1,72 @@
+namespace KnightFrank.Antares.Dal.Seed
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    using KnightFrank.Antares.Dal.Model;
+
+    internal static class EntityTypeEnum
+    {
+        public static void Seed(KnightFrankContext context)
+        {
+            SeedLocale(context);
+            SeedEnumType(context);
+            SeedEnumTypeItem(context);
+            SeedEnumLocalisation(context);
+        }
+
+        private static void SeedLocale(KnightFrankContext context)
+        {
+            var enLocale = new Locale { IsoCode = "en" };
+
+            context.Locale.AddOrUpdate(x => x.IsoCode, enLocale);
+            context.SaveChanges();
+        }
+
+        private static void SeedEnumType(KnightFrankContext context)
+        {
+            var entityTypeEnumType = new EnumType { Code = "EntityType" };
+
+            context.EnumType.AddOrUpdate(x => x.Code, entityTypeEnumType);
+            context.SaveChanges();
+        }
+
+        private static void SeedEnumTypeItem(KnightFrankContext context)
+        {
+            Guid entityTypeEnumTypeId = context.EnumType.Where(x => x.Code == "EntityType").Select(x => x.Id).Single();
+
+            var requirementEnumTypeItem = new EnumTypeItem { Code = "Requirement", EnumTypeId = entityTypeEnumTypeId };
+            var propertyEnumTypeItem = new EnumTypeItem { Code = "Property", EnumTypeId = entityTypeEnumTypeId };
+
+            context.EnumTypeItem.AddOrUpdate(x => x.Code, requirementEnumTypeItem, propertyEnumTypeItem);
+            context.SaveChanges();
+        }
+
+        private static void SeedEnumLocalisation(KnightFrankContext context)
+        {
+            Guid enLocaleId = context.Locale.Where(x => x.IsoCode == "en").Select(x=> x.Id).Single();
+            Guid requirementEnumTypeItemId = context.EnumTypeItem.Where(x => x.Code == "Requirement").Select(x => x.Id).Single();
+            Guid propertyEnumTypeItemId = context.EnumTypeItem.Where(x => x.Code == "Property").Select(x => x.Id).Single();
+
+            var entityTypeEnumLocalisations = new[]
+                                                  {
+                                                      new EnumLocalised
+                                                          {
+                                                              EnumTypeItemId = requirementEnumTypeItemId,
+                                                              LocaleId = enLocaleId,
+                                                              Value = "Requirement"
+                                                          },
+                                                      new EnumLocalised
+                                                          {
+                                                              EnumTypeItemId = propertyEnumTypeItemId,
+                                                              LocaleId = enLocaleId,
+                                                              Value = "Property"
+                                                          }
+                                                  };
+
+            context.EnumLocalised.AddOrUpdate(x => x.Value, entityTypeEnumLocalisations.ToArray());
+            context.SaveChanges();
+        }
+    }
+}
