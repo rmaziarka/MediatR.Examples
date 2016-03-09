1a8b5d8b2a55a94e4fa436ca1b4d090b127981f6
Ryszard Seniuta <rseniuta@objectivity.co.uk>
2016-03-09 14:59:21 +0100

    #20425 #20955 Extended create requirement command and handler

---
+++ src/KnightFrank.Antares.Domain/Requirement/Commands/CreateRequirementCommandValidator.cs
@@ -0,0 +1,26 @@
+namespace KnightFrank.Antares.Domain.Requirement.Commands
+{
+    using FluentValidation;
+
+    public class CreateRequirementCommandValidator : AbstractValidator<CreateRequirementCommand>
+    {
+        public CreateRequirementCommandValidator()
+        {
+            this.RuleFor(x => x.MaxPrice).GreaterThanOrEqualTo(x => x.MinPrice).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MaxBedrooms).GreaterThanOrEqualTo(x => x.MinBedrooms).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MaxReceptionRooms).GreaterThanOrEqualTo(x => x.MinReceptionRooms).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MaxBathrooms).GreaterThanOrEqualTo(x => x.MinBathrooms).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MaxParkingSpaces).GreaterThanOrEqualTo(x => x.MinParkingSpaces).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MaxArea).GreaterThanOrEqualTo(x => x.MinArea).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MaxLandArea).GreaterThanOrEqualTo(x => x.MinLandArea).GreaterThanOrEqualTo(0);
+
+            this.RuleFor(x => x.MinPrice).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MinBedrooms).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MinReceptionRooms).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MinBathrooms).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MinParkingSpaces).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MinArea).GreaterThanOrEqualTo(0);
+            this.RuleFor(x => x.MinLandArea).GreaterThanOrEqualTo(0);
+        }
+    }
+}
