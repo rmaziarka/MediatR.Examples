767da95b01ca0ef90a3d02d4a0430372c6ddb240
Marcin Biernacik <mbiernacik@objectivity.co.uk>
2016-03-11 11:43:57 +0100

    #21192 #21195 Dictionary database structure. Create enum controller for retrieving enums (support localisation) Query handler improvements - using Automapper.
    Reviewed by: Kamil Haladus

---
+++ src/KnightFrank.Antares.Domain/Enum/EnumQueryItemResult.cs
@@ -0,0 +1,11 @@
+﻿namespace KnightFrank.Antares.Domain.Enum
+{
+    using System;
+
+    public class EnumQueryItemResult
+    {
+        public Guid Id { get; set; }
+
+        public string Value { get; set; }
+    }
+}
