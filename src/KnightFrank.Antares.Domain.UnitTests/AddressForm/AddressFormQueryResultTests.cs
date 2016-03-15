9e5662d4b4c2f58ea705e068d3fe60631261b21d
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-03-15 10:31:03 +0100

    #20401 #21557 Unit tests for AddressFormQueryResult Reviewed by: Marcin Biernacik

---
+++ src/KnightFrank.Antares.Domain.UnitTests/AddressForm/AddressFormQueryResultTests.cs
@@ -0,0 +1,23 @@
+﻿namespace KnightFrank.Antares.Domain.UnitTests.AddressForm
+{
+    using FluentAssertions;
+
+    using KnightFrank.Antares.Domain.AddressForm;
+
+    using Xunit;
+
+    [Collection("Models")]
+    [Trait("FeatureTitle", "Models")]
+    public class AddressFormQueryResultTests
+    {
+        public void When_CreateAddressFormQueryResult_Then_AddressFieldDefinitionsShouldBeEmpty()
+        {
+            // Act
+            var addressFormQueryResult = new AddressFormQueryResult();
+
+            // Act
+            addressFormQueryResult.AddressFieldDefinitions.Should().NotBeNull();
+            addressFormQueryResult.AddressFieldDefinitions.Should().BeEmpty();
+        } 
+    }
+}
