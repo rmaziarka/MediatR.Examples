1f153da92890590e717a426b46ab613db3e6390b
Wojciech Machniak <wmachniak@objectivity.co.uk>
2016-03-17 12:01:14 +0100

    #20432 #21733 Add property view controller.
    Reviewed by: Radoslaw Maziarka

---
+++ src/wwwroot/app/property/view/propertyViewController.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.Property.View {
+    export class PropertyViewController {
+
+    }
+    angular.module('app').controller('propertyViewController', PropertyViewController);
+}
