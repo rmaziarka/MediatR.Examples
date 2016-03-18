197212cdd8e8a2d854eba32f48efd9f374fb4d25
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-03-18 16:00:32 +0100

    #20139 #20919 Add property tamplate view Reviewed by: Marta Dycjan

---
+++ src/wwwroot/app/property/add/propertyAddController.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.Property {
+    export class PropertyAddController {
+        public save() {
+            alert("Saved");
+        }      
+    }
+
+    angular.module('app').controller('PropertyAddController', PropertyAddController);
+}
