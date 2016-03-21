e93e19ac3c84fccc9cf61f125af2e08c3332309d
Michal Lenartowicz <mlenartowicz@objectivity.co.uk>
2016-03-21 10:42:33 +0100

    #20140 #21163 Create command to save ownership in to data base Reviewed by: Radek Maziarka

---
+++ src/KnightFrank.Antares.Domain/Ownership/Commands/CreateOwnershipCommandProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace KnightFrank.Antares.Domain.Ownership.Commands
+{
+    using AutoMapper;
+
+    using KnightFrank.Antares.Dal.Model;
+
+    public class CreateOwnershipCommandProfile : Profile
+    {
+        protected override void Configure()
+        {
+            this.CreateMap<CreateOwnershipCommand, Ownership>();
+        }
+    }
+}
