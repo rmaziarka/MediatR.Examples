0356af507a2a102663cdad694e85600cd8ba2e03
Wojciech Machniak <wmachniak@objectivity.co.uk>
2016-03-23 14:25:39 +0100

    #20433 #21754 Create activity view controller.
    Reviewed by: Ryszard Seniuta

---
+++ src/wwwroot/app/activity/view/activityViewController.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.Activity.View {
+    export class ActivityViewController {}
+
+    angular.module('app').controller('activityViewController', ActivityViewController);
+}
