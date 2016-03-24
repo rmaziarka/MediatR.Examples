c07cf853fdd92a8abc8e50a032e770f76e0f539b
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-03-24 10:30:02 +0100

    #20407 #21752 #21756 add property types db structure and seed divisions
    Reviewed by: Radoslaw Maziarka

---
+++ src/KnightFrank.Antares.Dal/Model/PropertyType/PropertyTypeDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KnightFrank.Antares.Dal.Model.PropertyType
+{
+    using KnightFrank.Antares.Dal.Model.Enum;
+
+    public class PropertyTypeDefinition : BaseEntity
+    {
+        public Guid PropertyTypeId { get; set; }
+        public PropertyType PropertyType { get; set; }
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+        public Guid DivisionId { get; set; }
+        public EnumTypeItem Division { get; set; }
+    }
+}
