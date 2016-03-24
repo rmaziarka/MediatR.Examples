38b9f1b19cbbc65f49d544ecd75d868c72e422dc
Marta Dycjan <martakanak@gmail.com>
2016-03-24 11:35:27 +0100

    #20139 Change returned value from property controller when POST/PUT
    Reviewed by: Zbigniew Rubinski

--- src/KnightFrank.Antares.Domain/Property/CommandHandlers/CreatePropertyCommandHandler.cs
+++ src/KnightFrank.Antares.Domain/Property/CommandHandlers/CreatePropertyCommandHandler.cs
@@ -1,14 +1,15 @@
 ﻿namespace KnightFrank.Antares.Domain.Property.CommandHandlers
 {
+    using System;
+
     using AutoMapper;
 
-    using KnightFrank.Antares.Dal.Model;
     using KnightFrank.Antares.Dal.Model.Property;
     using KnightFrank.Antares.Dal.Repository;
     using KnightFrank.Antares.Domain.Property.Commands;
 
     using MediatR;
-    public class CreatePropertyCommandHandler : IRequestHandler<CreatePropertyCommand, Property>
+    public class CreatePropertyCommandHandler : IRequestHandler<CreatePropertyCommand, Guid>
     {
         private readonly IGenericRepository<Property> propertyRepository;
 
@@ -17,14 +18,14 @@
             this.propertyRepository = propertyRepository;
         }
 
-        public Property Handle(CreatePropertyCommand message)
+        public Guid Handle(CreatePropertyCommand message)
         {
             var property = Mapper.Map<Property>(message);
 
             this.propertyRepository.Add(property);
             this.propertyRepository.Save();
 
-            return property;
+            return property.Id;
         }
     }
 }
