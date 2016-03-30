1ab0bf2dded62b820571b67e4d3adf92310d5e68
Bartosz Szatanik <bszatanik@objectivity.co.uk>
2016-03-30 09:56:08 +0200

    [#20140][#21023] Ownership details field validation unit tests Reviewed by: Michal Lenartowicz

--- src/wwwroot/app/ownership/add/tests/ownershipAddComponent.spec.ts
+++ src/wwwroot/app/ownership/add/tests/ownershipAddComponent.spec.ts
@@ -22,8 +22,8 @@ module Antares {
             $httpBackend: ng.IHttpBackendService) => {
 
             $http = $httpBackend;
-            $http.whenGET(/\/api\/enums\/OwnershipType\/items/).respond(() =>{
-                return [200, { "items" : [{ "id" : "1", "value" : "Freeholder" }, { "id" : "2", "value" : "Leaseholder" }] }];
+            $http.whenGET(/\/api\/enums\/OwnershipType\/items/).respond(() => {
+                return [200, { "items": [{ "id": "1", "value": "Freeholder" }, { "id": "2", "value": "Leaseholder" }] }];
             });
 
             scope = $rootScope.$new();
@@ -36,12 +36,48 @@ module Antares {
             assertValidator = new Antares.TestHelpers.AssertValidators(element, scope);
         }));
 
-        it('when ownership type value is missing then required message should be displayed', () => {
-            assertValidator.assertRequiredValidator(null, false, pageObjectSelectors.ownershipTypeSelector);
-        });
+        describe('when', () => {
+            describe('ownership type value is ', () => {
+                it('missing then required message should be displayed', () => {
+                    assertValidator.assertRequiredValidator(null, false, pageObjectSelectors.ownershipTypeSelector);
+                });
 
-        it('when ownership type value is not missing then required message should not be displayed', () => {
-            assertValidator.assertRequiredValidator('1', true, pageObjectSelectors.ownershipTypeSelector);
+                it('not missing then required message should not be displayed', () => {
+                    assertValidator.assertRequiredValidator('1', true, pageObjectSelectors.ownershipTypeSelector);
+                });
+            });
+            describe('ownership purchasing date is ', () => {
+                it('missing then required message should be displayed', () => {
+                    assertValidator.assertRequiredValidator(null, false, '[name=purchaseDate]');
+                });
+
+                it('invalid then required message should be displayed', () => {
+                    assertValidator.assertRequiredValidator('invalid date', false, '[name=purchaseDate]');
+                });
+
+                it('vaild then required message should not be displayed', () => {
+                    assertValidator.assertRequiredValidator('21-12-1984', true, '[name=purchaseDate]');
+                });
+            });
+            describe('ownership selling date is ', () => {
+
+                it('missing then required message should be displayed', () => {
+                    assertValidator.assertRequiredValidator(null, false, '[name=sellDate]');
+                });
+
+                it('ownership selling date is invalid then required message should be displayed', () => {
+                    assertValidator.assertRequiredValidator('invalid date', false, '[name=sellDate]');
+                });
+
+                it('ownership selling date is vaild then required message should not be displayed', () => {
+                    assertValidator.assertRequiredValidator('21-12-1984', true, '[name=sellDate]');
+                });
+            });
+            describe('ownership buying price is ', () => {
+                it('lower then zero', () => {
+                    assertValidator.assertMinValueValidator(-10, false, "[name=buyPrice]");
+                });
+            });
         });
     });
 }
