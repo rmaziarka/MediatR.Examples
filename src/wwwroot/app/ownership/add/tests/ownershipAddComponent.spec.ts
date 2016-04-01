caec1373e9c374585967a7a036083b6fe44fba8f
Michal Lenartowicz <mlenartowicz@objectivity.co.uk>
2016-04-01 10:33:14 +0200

    #22373 Validation tests on ownership details add Reviewed by: Lukasz Misztal

--- src/wwwroot/app/ownership/add/tests/ownershipAddComponent.spec.ts
+++ src/wwwroot/app/ownership/add/tests/ownershipAddComponent.spec.ts
@@ -9,7 +9,11 @@ module Antares {
             $http: ng.IHttpBackendService;
 
         var pageObjectSelectors = {
-            ownershipTypeSelector: 'select#type'
+            ownershipTypeSelector: 'select#type',
+            purchaseDateSelector: '[name=purchaseDate]',
+            sellDateSelector: '[name=sellDate]',
+            buyPriceSelector: '[name=buyPrice]',
+            sellPriceSelector: '[name=sellPrice]'
         };
 
         var controller: OwnershipAddController;
@@ -44,32 +48,66 @@ module Antares {
                 });
 
                 it('not missing then required message should not be displayed', () => {
-                    assertValidator.assertRequiredValidator('1', true, pageObjectSelectors.ownershipTypeSelector);
+                    assertValidator.assertRequiredValidator('1', true, pageObjectSelectors.ownershipTypeSelector);                    
                 });
             });
             describe('ownership purchasing date is ', () => {
                 it('invalid then required message should be displayed', () => {
-                    assertValidator.assertRequiredValidator('invalid date', false, '[name=purchaseDate]');
+                    assertValidator.assertRequiredValidator('invalid date', false, pageObjectSelectors.purchaseDateSelector);
                 });
 
                 it('vaild then required message should not be displayed', () => {
-                    assertValidator.assertRequiredValidator('21-12-1984', true, '[name=purchaseDate]');
+                    assertValidator.assertRequiredValidator('21-12-1984', true, pageObjectSelectors.purchaseDateSelector);
                 });
             });
             describe('ownership selling date is ', () => {
 
                 it('ownership selling date is invalid then required message should be displayed', () => {
-                    assertValidator.assertRequiredValidator('invalid date', false, '[name=sellDate]');
+                    assertValidator.assertRequiredValidator('invalid date', false, pageObjectSelectors.sellDateSelector);
                 });
 
-                it('ownership selling date is vaild then required message should not be displayed', () => {
-                    assertValidator.assertRequiredValidator('21-12-1984', true, '[name=sellDate]');
+                it('ownership selling date is valid then required message should not be displayed', () => {
+                    assertValidator.assertRequiredValidator('21-12-1984', true, pageObjectSelectors.sellDateSelector);
                 });
             });
             describe('ownership buying price is ', () => {
-                it('lower then zero', () => {
-                    assertValidator.assertMinValueValidator(-10, false, "[name=buyPrice]");
+                it('lower then zero then validation message should be displayed', () => {
+                    assertValidator.assertMinValueValidator(-10, false, pageObjectSelectors.buyPriceSelector);
                 });
+
+                it('in exponential notation then validation message should be displayed', () => {
+                    assertValidator.assertPatternValidator('10e2', false, pageObjectSelectors.buyPriceSelector);
+                });
+
+                it('valid positive number then no validation message should be displayed', () => {
+                    assertValidator.assertRequiredValidator('123', true, pageObjectSelectors.buyPriceSelector);
+                });
+
+                it('invalid string input then element should be empty and no validation message is displayed', () => {
+                    assertValidator.assertRequiredValidator('xyz', true, pageObjectSelectors.buyPriceSelector);
+                    var sellPriceElement = element.find(pageObjectSelectors.buyPriceSelector);
+                    expect(sellPriceElement.hasClass('ng-empty')).toBeTruthy();
+                });
+            });
+            describe('ownership sell price is ', () => {
+                it('lower then zero then validation message should be displayed', () => {
+                    assertValidator.assertMinValueValidator(-10, false, pageObjectSelectors.sellPriceSelector);
+                });
+
+                it('in exponential notation then validation message should be displayed', () => {
+                    assertValidator.assertPatternValidator('10e2', false, pageObjectSelectors.sellPriceSelector);
+                });
+
+                it('valid positive number then no validation message should be displayed', () => {
+                    assertValidator.assertRequiredValidator('123', true, pageObjectSelectors.sellPriceSelector);
+                });
+
+                it('invalid string input then element should be empty and no validation message is displayed', () => {
+                    assertValidator.assertRequiredValidator('xyz', true, pageObjectSelectors.sellPriceSelector);
+                    var sellPriceElement = element.find(pageObjectSelectors.sellPriceSelector);
+                    expect(sellPriceElement.hasClass('ng-empty')).toBeTruthy();
+                });
+                
             });
         });
     });
