9eeb3f8c7a5cc2be3dca23ed6fde00dd24a97b6d
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-04-01 14:46:46 +0200

    #21781 Add loading and showing activities on property view - unit test review by: Marcin Biernacik

---
+++ src/wwwroot/app/property/view/test/propertyViewComponent.spec.ts
@@ -0,0 +1,70 @@
+﻿/// <reference path="../../../typings/_all.d.ts" />
+
+module Antares {
+    import PropertyViewController = Property.View.PropertyViewController;
+    import IProperty = Antares.Common.Models.Dto.IProperty;
+    describe('Given view property page is loaded', () => {
+        var scope: ng.IScope,
+            element: ng.IAugmentedJQuery,
+            $http: ng.IHttpBackendService,
+            filter: ng.IFilterService;
+
+        var controller: PropertyViewController;
+
+        describe('and proper property id is provided', () => {
+            var propertyMock: IProperty = {
+                id: '1',
+                address: Antares.Mock.AddressForm.FullAddress,
+                ownerships: [],
+                activities: []
+            };
+
+            beforeEach(angular.mock.module('app'));
+            beforeEach(inject((
+                $rootScope: ng.IRootScopeService,
+                $compile: ng.ICompileService,
+                $httpBackend: ng.IHttpBackendService) =>{
+
+                $http = $httpBackend;
+
+                Antares.Mock.AddressForm.mockHttpResponce($http, 'a1', [200, Antares.Mock.AddressForm.AddressFormWithOneLine]);
+                $http.whenGET(/\/api\/enums\/.*\/items/).respond(() => {
+                    return [];
+                });
+
+                scope = $rootScope.$new();
+                scope['property'] = propertyMock;
+                element = $compile('<property-view property="property"></property-view>')(scope);
+
+                scope.$apply();
+                $http.flush();
+
+                controller = element.controller('propertyView');
+            }));
+
+            it('card-list component for activities is set up', () => {
+                var cardListElement = element.find('card-list');
+
+                var cardListHeaderElement = cardListElement.find('card-list-header');
+                var cardListHeaderElementContent = cardListHeaderElement.find('[translate="ACTIVITY.LIST.ACTIVITIES"]');
+
+                var cardListNoItemsElement = cardListElement.find('card-list-no-items');
+                var cardListNoItemsElementContent = cardListNoItemsElement.find('[translate="ACTIVITY.LIST.NO_ITEMS"]');
+
+                expect(cardListElement.length).toBe(1);
+                expect(cardListElement[0].getAttribute('card-template-url')).toBe("'app/activity/templates/activityCard.html'");
+                expect(cardListElement[0].getAttribute('items')).toBe("vm.property.activities");
+                expect(cardListElement[0].getAttribute('items-order')).toBe("'createdDate'");
+                expect(cardListElement[0].getAttribute('show-item-add')).toBe("vm.showActivityAdd");
+                expect(cardListElement[0].getAttribute('show-item-details')).toBe("vm.showActivityDetails");
+
+                expect(cardListHeaderElement.length).toBe(1);
+                expect(cardListHeaderElementContent.length).toBe(1);
+
+                expect(cardListNoItemsElement.length).toBe(1);
+                expect(cardListNoItemsElementContent.length).toBe(1);
+            });
+        });
+
+    });
+}
