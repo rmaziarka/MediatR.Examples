ec5d1f9fcae73556e84d0aa96e650cf71540f342
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-04-04 13:44:36 +0200

    #20433 #21783 Prepare web api for getting activity by id Reviewed by: Kamil Haladus

---
+++ src/KnightFrank.Antares.Domain/Activity/Queries/ActivityQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace KnightFrank.Antares.Domain.Activity.Queries
+{
+    using FluentValidation;
+
+    public class ActivityQueryValidator : AbstractValidator<ActivityQuery>
+    {
+        public ActivityQueryValidator()
+        {
+            this.RuleFor(q => q.Id).NotNull();
+        }
+    }
+}
