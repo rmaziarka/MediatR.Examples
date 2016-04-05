3db38db552164cb6cbc79f228e1e5d98125b6572
Kamil Haładus <khaladus@objectibity.co.uk>
2016-04-05 13:09:17 +0200

    #21794 #20142 Create logic for adding activity. Added Unit Test for cmd Handler. Reviewed by: Marcin Biernacik

--- src/KnightFrank.Antares.Domain.UnitTests/Activity/CommandHandlers/CreateActivityCommandHandlerTests.cs
+++ src/KnightFrank.Antares.Domain.UnitTests/Activity/CommandHandlers/CreateActivityCommandHandlerTests.cs
@@ -1,21 +1,54 @@
 ﻿namespace KnightFrank.Antares.Domain.UnitTests.Activity.CommandHandlers
 {
-    using KnightFrank.Antares.Domain.Activity.CommandHandlers;
+    using System;
 
-    using Ploeh.AutoFixture;
-    using Ploeh.AutoFixture.AutoMoq;
+    using FluentAssertions;
+    using FluentAssertions.Common;
+
+    using KnightFrank.Antares.Dal.Model.Contacts;
+    using KnightFrank.Antares.Dal.Model.Property;
+    using KnightFrank.Antares.Dal.Repository;
+    using KnightFrank.Antares.Domain.Activity.CommandHandlers;
+    using KnightFrank.Antares.Domain.Activity.Commands;
+
+    using Moq;
+
+    using Ploeh.AutoFixture.Xunit2;
 
     using Xunit;
-    public class CreateActivityCommandHandlerTests : IClassFixture<CreateActivityCommandHandler>
+    [Trait("FeatureTitle", "Property Activity")]
+    [Collection("CreateActivityCommandHandler")]
+    public class CreateActivityCommandHandlerTests : IClassFixture<BaseTestClassFixture>
     {
-        private readonly IFixture fixture;
+        [Theory]
+        [AutoMoqData]
+        public void Given_ValidCommand_When_Handling_Then_ShouldSaveActivity(
+            [Frozen] Mock<IGenericRepository<Activity>> activityRepository,
+            [Frozen] Mock<IGenericRepository<Contact>> contactRepository,
+            CreateActivityCommandHandler handler, 
+            CreateActivityCommand cmd,
+            Guid expectedActivityId)
+        {   
+            // Arrange 
+            Activity activity = null;
+            activityRepository.Setup(r => r.Add(It.IsAny<Activity>()))
+                              .Returns((Activity a) =>
+                              {
+                                  activity = a;
+                                  return activity;
+                              });
+            activityRepository.Setup(r => r.Save()).Callback(() => { activity.Id = expectedActivityId; });
+            
+            // Act
+            handler.Handle(cmd);
 
-        public CreateActivityCommandHandlerTests()
-        {
-            this.fixture = new Fixture().Customize(new AutoMoqCustomization());
-            this.fixture.Behaviors.Clear();
-            this.fixture.RepeatCount = 1;
-            this.fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            // Assert
+            activity.Should().NotBeNull();
+            activity.ShouldBeEquivalentTo(cmd,
+                opt => opt.IncludingProperties().ExcludingMissingMembers());
+            activity.Id.ShouldBeEquivalentTo(expectedActivityId);
+            activityRepository.Verify(r => r.Add(It.IsAny<Activity>()), Times.Once());
+            activityRepository.Verify(r => r.Save(), Times.Once());
         }
     }
 }
