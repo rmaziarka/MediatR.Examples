7667c050dfaea66e280acb07b569ac3d60d3378c
Ryszard Seniuta <rseniuta@objectivity.co.uk>
2016-04-05 12:59:48 +0200

    #21312 #22485 Added min max columns to Property

--- src/KnightFrank.Antares.Dal/Model/Property/Property.cs
+++ src/KnightFrank.Antares.Dal/Model/Property/Property.cs
@@ -18,5 +18,22 @@
         public Guid PropertyTypeId { get; set; }
         
         public virtual PropertyType PropertyType { get; set; }
+
+        public int? MinBedrooms { get; set; }
+        public int? MaxBedrooms { get; set; }
+        public int? MinReceptions { get; set; }
+        public int? MaxReceptions { get; set; }
+        public int? MinBathrooms { get; set; }
+        public int? MaxBathrooms { get; set; }
+        public double? MinArea { get; set; }
+        public double? MaxArea { get; set; }
+        public double? MinLandArea { get; set; }
+        public double? MaxLandArea { get; set; }
+        public int? MinGuestRooms { get; set; }
+        public int? MaxGuestRooms { get; set; }
+        public int? MinFunctionRooms { get; set; }
+        public int? MaxFunctionRooms { get; set; }
+        public int? MinCarParkingSpaces { get; set; }
+        public int? MaxCarParkingSpaces { get; set; }
     }
 }
