131f2f6e28ed2cceb60e9904fc20b75af239063d
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-04-05 12:08:36 +0200

    #20433 #21790 Create generic component for viewing simple lists Review by: Marta Dycjan

---
+++ src/wwwroot/app/common/components/list/listComponent.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../../typings/_all.d.ts" />
+
+module Antares.Common.Component {
+    angular.module('app').component('list', {
+        templateUrl: 'app/common/components/list/list.html',
+        transclude: {
+            'header': '?listHeader',
+            'noItems': '?listNoItems'
+        }
+    });
+}
