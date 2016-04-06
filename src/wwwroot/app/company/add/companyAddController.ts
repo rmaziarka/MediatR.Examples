f52d2a653002ffb2394e93b6a09eba580595bbc4
Wojciech Machniak <wmachniak@objectivity.co.uk>
2016-04-06 15:28:05 +0200

    #21995 #22463 Create HTML template & Create company controller.
    Reviewed by: Marcin Biernacik

---
+++ src/wwwroot/app/company/add/companyAddController.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.Company {
+    export class CompanyAddController {}
+
+    angular.module('app').controller('CompanyAddController', CompanyAddController);
+}
---
+++ src/wwwroot/app/company/company.route.ts
@@ -0,0 +1,17 @@
+/// <reference path="../typings/_all.d.ts" />
+
+module Antares.Company {
+    var app: ng.IModule = angular.module('app');
+
+    app.config(initRoute);
+
+    function initRoute($stateProvider: ng.ui.IStateProvider) {
+        $stateProvider
+            .state('app.company-add', {
+                url: '/company/add',
+                templateUrl: 'app/company/add/companyAdd.html',
+                controllerAs: 'vm',
+                controller: 'companyAddController'
+            });
+    }
+}
