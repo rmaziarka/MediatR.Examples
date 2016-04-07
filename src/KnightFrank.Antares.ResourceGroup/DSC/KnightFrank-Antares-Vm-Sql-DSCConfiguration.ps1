f1c29fe0c82b05a64e320618544a80f9dabd1f5d
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-04-07 15:30:40 +0200

    #22085 #22654 #22852 (un)mount share and iso; join vm to domain (commented)
    Reviewed by: not yet reviewed

--- src/KnightFrank.Antares.ResourceGroup/DSC/KnightFrank-Antares-Vm-Sql-DSCConfiguration.ps1
+++ src/KnightFrank.Antares.ResourceGroup/DSC/KnightFrank-Antares-Vm-Sql-DSCConfiguration.ps1
@@ -1,100 +1,72 @@
 Configuration SetupSQLVm
 {
 
-Param ( [string] $nodeName,
-		
-		[Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $PackagePath,
- 
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $WinSources )
+Param ( [string] $nodeName )
 
 Import-DscResource -ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName xSQLServer
+Import-DscResource -ModuleName xStorage
+
+$isoFileName = "en_sql_server_2014_developer_edition_with_service_pack_1_x86_dvd_6668541.iso";
 
 Node $nodeName
 {
-    Log ParamLog
-    {
-        Message = "Running SQLInstall. PackagePath = $PackagePath"
-    }
- 
-    WindowsFeature NetFramework35Core
-    {
-        Name = "NET-Framework-Core"
-        Ensure = "Present"
-        Source = $WinSources
-    }
- 
-    WindowsFeature NetFramework45Core
-    {
-        Name = "NET-Framework-45-Core"
-        Ensure = "Present"
-        Source = $WinSources
-    }
- 
-    # copy the sqlserver iso
-    File SQLServerIso
-    {
-        SourcePath = "$PackagePath\en_sql_server_2014_developer_edition_x86_x64_dvd_813280.iso"
-        DestinationPath = "c:\temp\SQLServer.iso"
-        Type = "File"
-        Ensure = "Present"
-    }
- 
-    # copy the ini file to the temp folder
-    File SQLServerIniFile
-    {
-        SourcePath = "$PackagePath\ConfigurationFile.ini"
-        DestinationPath = "c:\temp"
-        Type = "File"
-        Ensure = "Present"
-        DependsOn = "[File]SQLServerIso"
-    }
- 
-    #
-    # Install SqlServer using ini file
-    #
-    Script InstallSQLServer
-    {
-        GetScript =
-        {
-            $sqlInstances = gwmi win32_service -computerName localhost | ? { $_.Name -match "mssql*" -and $_.PathName -match "sqlservr.exe" } | % { $_.Caption }
-            $res = $sqlInstances -ne $null -and $sqlInstances -gt 0
-            $vals = @{
-                Installed = $res;
-                InstanceCount = $sqlInstances.count
-            }
-            $vals
-        }
-        SetScript =
-        {
-            # mount the iso
-            $setupDriveLetter = (Mount-DiskImage -ImagePath c:\temp\SQLServer.iso -PassThru | Get-Volume).DriveLetter + ":"
-            if ($setupDriveLetter -eq $null) {
-                throw "Could not mount SQL install iso"
-            }
-            Write-Verbose "Drive letter for iso is: $setupDriveLetter"
-                 
-            # run the installer using the ini file
-            $cmd = "$setupDriveLetter\Setup.exe /ConfigurationFile=c:\temp\ConfigurationFile.ini /SQLSVCPASSWORD=P2ssw0rd /AGTSVCPASSWORD=P2ssw0rd /SAPWD=P2ssw0rd"
-            Write-Verbose "Running SQL Install - check %programfiles%\Microsoft SQL Server\120\Setup Bootstrap\Log\ for logs..."
-            Invoke-Expression $cmd | Write-Verbose
-        }
-        TestScript =
-        {
-            $sqlInstances = gwmi win32_service -computerName localhost | ? { $_.Name -match "mssql*" -and $_.PathName -match "sqlservr.exe" } | % { $_.Caption }
-            $res = $sqlInstances -ne $null -and $sqlInstances -gt 0
-            if ($res) {
-                Write-Verbose "SQL Server is already installed"
-            } else {
-                Write-Verbose "SQL Server is not installed"
-            }
-            $res
-        }
-    }
+	Script MapIsoShare
+	{
+		TestScript = { Test-Path "K:\" }
+		GetScript = {@{Result = "MountIso"}}
+		SetScript =
+		{
+			$accountName = "kfantarescommon"
+			$accountKey = ConvertTo-SecureString "IQURddUmGFAe1d3/+OC2Ay1gT9YfdwPi66Nzhy+3vWKgCcy6YA02i3EfLStunvNjCw4M9RW7pxe6dL5z3Y0X9w==" -AsPlainText -Force
+			$credential = new-object -TypeName System.Management.Automation.PSCredential -ArgumentList $accountName, $accountKey
+			New-PSDrive -Name K -Root \\kfantarescommon.file.core.windows.net\iso -Credential $credential -PSProvider FileSystem
+		}
+	}
+
+	File CopySqlIso
+	{
+		DependsOn = "[Script]MapIsoShare"
+		Ensure = "Present"
+		SourcePath = "k:\en_sql_server_2014_developer_edition_with_service_pack_1_x86_dvd_6668541.iso"
+		DestinationPath = "c:\temp\en_sql_server_2014_developer_edition_with_service_pack_1_x86_dvd_6668541.iso"
+		Type = "File"
+		MatchSource = $true
+		Force = $true
+	}
+	
+	Script UnMapIsoShare 
+	{
+		DependsOn = "[File]Test"	
+		TestScript = { -Not (Test-Path "K:\") }
+		GetScript = {@{Result = "UnMountIso"}}
+		SetScript =
+		{
+			Remove-PSDrive -Name K -Force
+		}
+	}
+
+	xMountImage MountSqlIso
+	{
+		DependsOn = "[File]CopySqlIso"
+		Name = "SQL Disc"
+		ImagePath = "c:\temp\$isoFileName"
+		DriveLetter = "s:"
+		Ensure = "Present"
+	}
+
+	xSqlServerSetup InstallSql
+	{
+		DependsOn = "[xMountImage]MountSqlIso"
+		SourcePath = "s:"
+	}
+
+	xMountImage UnMountSqlIso
+	{
+		DependsOn = "[xSqlServerSetup]InstallSql"
+		ImagePath = 'c:\temp\$isoFileName'
+        DriveLetter = 's:'
+        Ensure = 'Absent'
+	}
 }
 }
