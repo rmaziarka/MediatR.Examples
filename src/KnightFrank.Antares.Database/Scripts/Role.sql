9c920b4eeb6f45373cfc3a4f82af76db4d2e8bf8
Ryszard Seniuta <rseniuta@objectivity.co.uk>
2016-04-08 08:23:04 +0200

    Merging temporary table into Role table

--- src/KnightFrank.Antares.Database/Scripts/Role.sql
+++ src/KnightFrank.Antares.Database/Scripts/Role.sql
@@ -1,9 +1,28 @@
-﻿BULK INSERT dbo.Role
+﻿CREATE TABLE #TempRoles (      
+	[Id] [uniqueidentifier] NOT NULL DEFAULT (newsequentialid()),
+	[Name] [nvarchar](100) NULL,
+);
+
+BULK INSERT #TempRoles
     FROM '$(OutputPath)\Scripts\Data\Configuration\roles.csv'
     WITH
     (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ';',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ';',  --CSV field delimiter
+		ROWTERMINATOR = '\n',   --Use to shift the control to next row
+		TABLOCK
     )
+
+MERGE dbo.Role AS T
+USING #TempRoles AS S
+ON (T.Id = S.Id)
+       WHEN NOT MATCHED BY TARGET
+              THEN   INSERT(Id, Name) 
+                           VALUES(Id, Name)
+       WHEN MATCHED 
+              THEN   UPDATE SET
+                                  T.Name = S.Name
+       WHEN NOT MATCHED BY SOURCE
+              THEN DELETE;
+
+DROP TABLE #TempRoles
