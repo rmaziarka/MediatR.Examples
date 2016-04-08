64a04c80db0de3cd4b91469576d9555ce8ef9182
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-04-08 12:55:37 +0200

    #21995 #22434 Extend generator for contacts Reviewed by: Karolina Czerniawska

--- src/wwwroot/app/common/testHelpers/contactGenerator.test.ts
+++ src/wwwroot/app/common/testHelpers/contactGenerator.test.ts
@@ -1,10 +1,11 @@
 ﻿/// <reference path="../../typings/_all.d.ts" />
 
 module Antares.TestHelpers {
+    import Business = Antares.Common.Models.Business;
     import Dto = Antares.Common.Models.Dto;
 
     export class ContactGenerator {
-        public static Generate(): Dto.IContact {
+        public static GenerateDto(): Dto.IContact {
 
             var contact: Dto.IContact = {
                 firstName: ContactGenerator.makeRandom('firstName'),
@@ -16,8 +17,16 @@ module Antares.TestHelpers {
             return contact;
         }
 
-        public static GenerateMany(n: number): Dto.IContact[] {
-            return _.times(n, ContactGenerator.Generate);
+        public static GenerateManyDtos(n: number): Dto.IContact[] {
+            return _.times(n, ContactGenerator.GenerateDto);
+        }
+
+        public static GenerateMany(n: number): Business.Contact[] {
+            return _.map(ContactGenerator.GenerateManyDtos(n), (activity: Dto.IContact) => { return new Business.Contact(activity); });
+        }
+
+        public static Generate(): Business.Contact {
+            return new Business.Contact(ContactGenerator.GenerateDto());
         }
 
         private static makeRandom(text: string): string {
