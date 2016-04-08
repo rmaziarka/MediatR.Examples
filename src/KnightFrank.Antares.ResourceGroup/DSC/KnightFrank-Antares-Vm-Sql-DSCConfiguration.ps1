4d59e2f95513e2aa2a7b5eacdb2150f05e81f73e
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-04-08 14:08:39 +0200

    #22085 #22654 #22852 disallow concurrent DSC provisioning on different machines; fix mapping share and mounting iso
    Reviewed by: not yet reviewed

--- src/KnightFrank.Antares.ResourceGroup/DSC/KnightFrank-Antares-Vm-Sql-DSCConfiguration.ps1
+++ src/KnightFrank.Antares.ResourceGroup/DSC/KnightFrank-Antares-Vm-Sql-DSCConfiguration.ps1
@@ -7,45 +7,58 @@ Import-DscResource -ModuleName PSDesiredStateConfiguration
 Import-DscResource -ModuleName xSQLServer
 Import-DscResource -ModuleName xStorage
 
-$isoFileName = "en_sql_server_2014_developer_edition_with_service_pack_1_x86_dvd_6668541.iso";
-
+$isoFileName = "en_sql_server_2014_developer_edition_with_service_pack_1_x86_dvd_6668541.iso"
+$sqlInstanceName = "MSSQLSERVER"
+$sqlFeatures = "SQLENGINE,IS,SSMS,ADV_SSMS"    
+<#
+$sqlUserName = "KnightFrank.Antares.Db.User"
+$sqlUserPassword = ConvertTo-SecureString -AsPlainText '$Kf@admin' -Force
+$sqlUserCredential = new-object -typename System.Management.Automation.PSCredential -argumentlist $sqlUserName, $sqlUserPassword
+#>
 Node $nodeName
 {
+	PSDscAllowPlainTextPassword = $true
+	LocalConfigurationManager
+    {
+        RebootNodeIfNeeded = $true
+		ActionAfterReboot = "ContinueConfiguration"
+    }
+
 	Script MapIsoShare
 	{
-		TestScript = { Test-Path "K:\" }
-		GetScript = {@{Result = "MountIso"}}
+		TestScript = { 
+			$isoExists = Test-Path "c:\temp\$isoFileName"
+			$shareMounted = Test-Path "k:\"
+			$isoExists -or $shareMounted 
+		}
+		GetScript = {@{Result = "MapIsoShare"}}
 		SetScript =
 		{
-			$accountName = "kfantarescommon"
-			$accountKey = ConvertTo-SecureString "IQURddUmGFAe1d3/+OC2Ay1gT9YfdwPi66Nzhy+3vWKgCcy6YA02i3EfLStunvNjCw4M9RW7pxe6dL5z3Y0X9w==" -AsPlainText -Force
-			$credential = new-object -TypeName System.Management.Automation.PSCredential -ArgumentList $accountName, $accountKey
-			New-PSDrive -Name K -Root \\kfantarescommon.file.core.windows.net\iso -Credential $credential -PSProvider FileSystem
+			net use K: \\kfantarescommon.file.core.windows.net\iso /u:kfantarescommon IQURddUmGFAe1d3/+OC2Ay1gT9YfdwPi66Nzhy+3vWKgCcy6YA02i3EfLStunvNjCw4M9RW7pxe6dL5z3Y0X9w==	
 		}
 	}
-
+	
 	File CopySqlIso
 	{
 		DependsOn = "[Script]MapIsoShare"
 		Ensure = "Present"
-		SourcePath = "k:\en_sql_server_2014_developer_edition_with_service_pack_1_x86_dvd_6668541.iso"
-		DestinationPath = "c:\temp\en_sql_server_2014_developer_edition_with_service_pack_1_x86_dvd_6668541.iso"
+		SourcePath = "k:\$isoFileName"
+		DestinationPath = "c:\temp\$isoFileName"
 		Type = "File"
 		MatchSource = $true
-		Force = $true
 	}
 	
 	Script UnMapIsoShare 
 	{
-		DependsOn = "[File]Test"	
-		TestScript = { -Not (Test-Path "K:\") }
-		GetScript = {@{Result = "UnMountIso"}}
+		DependsOn = "[File]CopySqlIso"	
+		TestScript = { -Not (Test-Path "k:\") }
+		GetScript = {@{Result = "UnMapIsoShare"}}
 		SetScript =
 		{
-			Remove-PSDrive -Name K -Force
+			net use K: /delete
 		}
 	}
-
+	
 	xMountImage MountSqlIso
 	{
 		DependsOn = "[File]CopySqlIso"
@@ -54,13 +67,49 @@ Node $nodeName
 		DriveLetter = "s:"
 		Ensure = "Present"
 	}
-
+	<#
 	xSqlServerSetup InstallSql
 	{
 		DependsOn = "[xMountImage]MountSqlIso"
-		SourcePath = "s:"
+		SourcePath = "s:\"
+		InstallSharedDir = "C:\Program Files\Microsoft SQL Server"
+        InstallSharedWOWDir = "C:\Program Files (x86)\Microsoft SQL Server"
+        InstanceDir = "C:\Program Files\Microsoft SQL Server"
+        InstallSQLDataDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
+        SQLUserDBDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
+        SQLUserDBLogDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
+        SQLTempDBDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
+        SQLTempDBLogDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
+        SQLBackupDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
+		InstanceName = $sqlInstanceName
+		SecurityMode = "Mixed"
 	}
+	<#
+	xSqlServerFirewall SetFirewallRules
+    {
+        DependsOn = ("[xSqlServerSetup]InstallSql")
+        SourcePath = "s:\"
+        InstanceName = $sqlInstanceName
+        Features = $sqlFeatures
+    }
 
+	xSQLServerLogin SetupSqlLogin
+    {
+        DependsOn = ("[xSqlServerSetup]InstallSql")
+        Ensure = "Present"
+        Name = $sqlUserName
+        LoginType = "SQLLogin"
+		LoginCredential = $sqlUserCredential
+    }
+
+	xSQLServerDatabaseRole SetupSqlUser
+    {
+        DependsOn = ("[xSqlServerSetup]InstallSql")
+        Ensure = "Present"
+        Name = $sqlUserName
+        Role = "dbcreator"
+    }
+	
 	xMountImage UnMountSqlIso
 	{
 		DependsOn = "[xSqlServerSetup]InstallSql"
@@ -68,5 +117,6 @@ Node $nodeName
         DriveLetter = 's:'
         Ensure = 'Absent'
 	}
+	#>
 }
 }
