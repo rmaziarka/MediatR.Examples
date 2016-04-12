26bbfd30e14bc4db4cdd169055859d3b9a60c763
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-04-12 12:33:04 +0200

    #22893 Fixed classes and interfaces structure for country localized Review by: Zbigniew Rubiński

---
+++ src/wwwroot/app/common/models/business/resources/countryLocalised.ts
@@ -0,0 +1,7 @@
+﻿module Antares.Common.Models.Business {
+    export class CountryLocalised implements Dto.ICountryLocalised{
+        public country: Country;
+        public locale: Locale;
+        public value: string;
+    }
+}
