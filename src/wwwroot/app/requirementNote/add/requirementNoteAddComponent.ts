25771e13eda243dd77aa79462fce9e1cf5e8b135
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-04-15 15:11:24 +0200

    #20573 #22496 Added new folder and empty component for requirement add Review by: Marcin Biernacik

---
+++ src/wwwroot/app/requirementNote/add/requirementNoteAddComponent.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.RequirementNote
+{
+    angular.module('app').component('', {
+        templateUrl : '',
+        controllerAs : 'vm',
+        controller : ''
+    });
+}
---
+++ src/wwwroot/app/requirementNote/add/requirementNoteAddController.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.RequirementNote
+{
+    import Dto = Common.Models.Dto;
+    import Business = Common.Models.Business;
+
+    export class RequirementNoteAddController {
+    }
+
+    angular.module('app').controller('RequirementNoteAddController', RequirementNoteAddController);
+};
