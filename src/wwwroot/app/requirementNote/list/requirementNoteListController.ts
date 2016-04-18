b0e841ee2ecb64fc393a611cc8461df9d30a7fe1
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-04-18 10:40:04 +0200

    #20573 #22496 Added ordering for requirement notes list component + tests Review by: Zbigniew Rubiński

--- src/wwwroot/app/requirementNote/list/requirementNoteListController.ts
+++ src/wwwroot/app/requirementNote/list/requirementNoteListController.ts
@@ -1,13 +1,15 @@
 ﻿/// <reference path="../../typings/_all.d.ts" />
 
 module Antares.RequirementNote {
-    import Dto = Common.Models.Dto;
     import Business = Common.Models.Business;
+    import ListOrder = Common.Component.ListOrder;
 
     export class RequirementNoteListController {
 
         componentId: string;
         requirement: Business.Requirement;
+
+        noteListOrder: ListOrder = new ListOrder('createdDate', true);
 
         constructor(
             componentRegistry: Core.Service.ComponentRegistry) {
