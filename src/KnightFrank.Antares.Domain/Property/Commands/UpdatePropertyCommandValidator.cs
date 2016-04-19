8259590da9fb24795edee7b3851ad63ee0014fde
Radek Maziarka <rmaziarka@objectivity.co.uk>
2016-04-19 11:33:08 +0200

    #20406 #22508 Add validation to passed attribute values in web api Reviewed by Łukasz Misztal

--- src/KnightFrank.Antares.Domain/Property/Commands/UpdatePropertyCommandValidator.cs
+++ src/KnightFrank.Antares.Domain/Property/Commands/UpdatePropertyCommandValidator.cs
@@ -1,6 +1,9 @@
 ﻿namespace KnightFrank.Antares.Domain.Property.Commands
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
 
     using FluentValidation;
     using FluentValidation.Results;
@@ -10,20 +13,25 @@
     using KnightFrank.Antares.Dal.Model.Property;
     using KnightFrank.Antares.Dal.Repository;
 
+    using Attribute = KnightFrank.Antares.Dal.Model.Attribute.Attribute;
+
     public class UpdatePropertyCommandValidator : AbstractValidator<UpdatePropertyCommand>
     {
         private readonly IGenericRepository<EnumTypeItem> enumTypeItemRepository;
         private readonly IGenericRepository<PropertyTypeDefinition> propertyTypeDefinitionRepository;
+        private readonly IGenericRepository<PropertyAttributeForm> propertyAttributeFormRepository;
 
         public UpdatePropertyCommandValidator(
             IGenericRepository<AddressFieldDefinition> addressFieldDefinitionRepository,
             IGenericRepository<AddressForm> addressFormRepository,
             IGenericRepository<PropertyType> propertyTypeRepository,
             IGenericRepository<PropertyTypeDefinition> propertyTypeDefinitionRepository,
-            IGenericRepository<EnumTypeItem> enumTypeItemRepository)
+            IGenericRepository<EnumTypeItem> enumTypeItemRepository,
+            IGenericRepository<PropertyAttributeForm> propertyAttributeFormRepository)
         {
             this.enumTypeItemRepository = enumTypeItemRepository;
             this.propertyTypeDefinitionRepository = propertyTypeDefinitionRepository;
+            this.propertyAttributeFormRepository = propertyAttributeFormRepository;
 
             this.RuleFor(v => v.Id).NotNull();
             this.RuleFor(x => x.Address).NotNull();
@@ -35,6 +43,7 @@
             this.RuleFor(x => x.DivisionId).NotEqual(Guid.Empty);
             this.Custom(this.DivisionExists);
             this.Custom(this.PropertyTypeIsValid);
+            this.Custom(this.AttributeValuesAreAllowedForPropertyType);
         }
 
         private ValidationFailure DivisionExists(UpdatePropertyCommand updatePropertyCommand)
@@ -53,6 +62,40 @@
                                                                && p.DivisionId.Equals(updatePropertyCommand.DivisionId)
                                                                && p.CountryId.Equals(updatePropertyCommand.Address.CountryId));
             return !propertyTypeDefinitionExist ? new ValidationFailure(nameof(updatePropertyCommand.PropertyTypeId), "Specified property type is invalid") : null;
+        }
+
+        private ValidationFailure AttributeValuesAreAllowedForPropertyType(UpdatePropertyCommand updatePropertyCommand)
+        {
+            IEnumerable<Attribute> allowedAttributes = this.propertyAttributeFormRepository
+                .GetWithInclude(p => p.PropertyTypeId == updatePropertyCommand.PropertyTypeId && p.Country.IsoCode == "GB",
+                    p => p.PropertyAttributeFormDefinitions.Select(s => s.Attribute))
+                .Single()
+                .PropertyAttributeFormDefinitions
+                .Select(s => s.Attribute);
+
+            List<string> allowedAttributeNames = allowedAttributes.Select(a => a.NameKey).ToList();
+
+            PropertyInfo[] attributeValueProperties = typeof(CreateOrUpdatePropertyAttributeValues)
+                .GetProperties();
+
+            List<string> attributeValueNames = attributeValueProperties.Select(a => a.Name).ToList();
+
+            IEnumerable<string> allowedAttributeValues =
+                from attributeValueName in attributeValueNames
+                from allowedAttributeName in allowedAttributeNames
+                where attributeValueName == "Min" + allowedAttributeName
+                   || attributeValueName == "Max" + allowedAttributeName
+                select attributeValueName;
+
+            IEnumerable<object> notAllowedAttributeValues = attributeValueProperties
+                .Where(propertyInfo => !allowedAttributeValues.Contains(propertyInfo.Name))
+                .Select(propertyInfo => propertyInfo.GetValue(updatePropertyCommand.AttributeValues));
+
+            bool allNotAllowedAttributeValuesAreNull = notAllowedAttributeValues.All(value => value == null);
+
+            return allNotAllowedAttributeValuesAreNull
+                ? null
+                : new ValidationFailure(nameof(updatePropertyCommand.AttributeValues), "Invalid attributes values are defined.");
         }
     }
 }
