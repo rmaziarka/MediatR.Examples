f238f5509054a438d557ee8e328b05e412493dfc
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-04-20 15:52:38 +0200

    #21983 #23887 fix showing/hiding range attribute elements
    Reviewed by: Michal Lenartowicz

--- src/wwwroot/app/common/components/attribute/range/rangeAttributeController.ts
+++ src/wwwroot/app/common/components/attribute/range/rangeAttributeController.ts
@@ -7,6 +7,17 @@ module Antares.Common.Component {
         public minValue: number;
         public maxValue: number;
         public attribute: Business.Attribute;
+        
+        get showMaxPrefix(){
+            return this.minValue == undefined && angular.isNumber(this.maxValue);
+        };
+        get showMinPrefix(){
+            return this.maxValue == undefined && angular.isNumber(this.minValue);
+        }
+        get showHyphen(){
+            return (angular.isNumber(this.minValue) && angular.isNumber(this.maxValue) && this.minValue !== this.maxValue) || (this.minValue == undefined && this.maxValue == undefined);
+        }
+
         mode: string = 'edit';
 
         getTemplateUrl(){
