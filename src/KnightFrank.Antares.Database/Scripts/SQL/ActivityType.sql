998a48c26c6ee00512679dd4ab987aa9d649e98a
Łukasz Misztal <lmisztal@objectivity.co.uk>
2016-04-21 14:03:07 +0200

    .

---
+++ src/KnightFrank.Antares.Database/Scripts/SQL/ActivityType.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE #TempActivityType (
+	[Code] NVARCHAR (50) NOT NULL
+);
+
+BULK INSERT #TempActivityType
+    FROM '$(OutputPath)\Scripts\Data\Configuration\ActivityType.csv'
+               WITH
+    (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ';',
+		ROWTERMINATOR = '\n',
+		TABLOCK
+    )
+
+ALTER TABLE ActivityType NOCHECK CONSTRAINT ALL
+	    	
+MERGE ActivityType AS T
+	USING #TempActivityType
+	AS S	
+	ON 
+	(
+        (T.Code = S.Code)
+	)
+	WHEN MATCHED THEN
+		UPDATE SET
+		T.[Code] = S.[Code]
+
+	WHEN NOT MATCHED BY TARGET THEN 
+		INSERT ([Code])
+		VALUES ([Code])
+
+    WHEN NOT MATCHED BY SOURCE THEN
+		DELETE;
+
+ALTER TABLE ActivityType WITH CHECK CHECK CONSTRAINT ALL
+DROP TABLE #TempActivityType
