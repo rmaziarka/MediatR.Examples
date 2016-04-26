86139297d98aa8c2bada5db54235d8cb717d2b5e
Łukasz Misztal <lmisztal@objectivity.co.uk>
2016-04-26 14:15:36 +0200

    #22717 #23389  Create entity model based on db document Reviewed by: Radosław Maziarka

--- BuildDeploy/deploy.bat
+++ BuildDeploy/deploy.bat
@@ -1,3 +1,3 @@
 @pushd %~dp0
-@powershell -Command "&{.\Deploy.ps1}"
+@powershell -Command "&{.\Deploy.ps1 -dropexistingdatabase}"
 @pause
---
+++ src/KnightFrank.Antares.Dal/Model/Property/Viewing.cs
@@ -0,0 +1,29 @@
+﻿namespace KnightFrank.Antares.Dal.Model.Property
+{
+    using System;
+    using System.Collections.Generic;
+
+    using KnightFrank.Antares.Dal.Model.Contacts;
+    using Activities;
+
+    public class Viewing : BaseEntity
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string InvitationText { get; set; }
+
+        public string PostViewingComment { get; set; }
+
+        public Guid ActivityId { get; set; }
+
+        public virtual Activity Activity { get; set; }
+
+        public Guid RequirementId { get; set; }
+
+        public virtual Requirement Requirement { get; set; }
+
+        public List<Contact> Attendees { get; set; } = new List<Contact>();
+    }
+}
