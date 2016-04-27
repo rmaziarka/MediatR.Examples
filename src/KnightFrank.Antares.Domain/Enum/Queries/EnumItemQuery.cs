d99e87111cfd4dfd9de47572a3a02cdf63f8492a
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-04-27 11:07:35 +0200

    #23284 #22584 Dictionary of enum type items Reviewed by: Marcin Biernacik

---
+++ src/KnightFrank.Antares.Domain/Enum/Queries/EnumItemQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace KnightFrank.Antares.Domain.Enum.Queries
+{
+    using System.Collections.Generic;
+
+    using KnightFrank.Antares.Domain.Enum.QueryResults;
+
+    using MediatR;
+
+    public class EnumItemQuery : IRequest<Dictionary<string, ICollection<EnumItemResult>>>
+    {
+    }
+}
---
+++ src/Tests/KnightFrank.Antares.Domain.UnitTests/Enum/QueryHandlers/EnumItemQueryHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿namespace KnightFrank.Antares.Domain.UnitTests.Enum.QueryHandlers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using AutoMapper;
+
+    using FluentAssertions;
+
+    using KnightFrank.Antares.Dal.Model.Enum;
+    using KnightFrank.Antares.Dal.Repository;
+    using KnightFrank.Antares.Domain.Enum.Queries;
+    using KnightFrank.Antares.Domain.Enum.QueryHandlers;
+    using KnightFrank.Antares.Domain.Enum.QueryResults;
+
+    using Moq;
+
+    using Ploeh.AutoFixture.Xunit2;
+
+    using Xunit;
+
+    [Collection("EnumItemQueryHandler")]
+    [Trait("FeatureTitle", "Enums")]
+    public class EnumItemQueryHandlerTests : IClassFixture<BaseTestClassFixture>
+    {
+        [Theory]
+        [AutoMoqData]
+        public void Given_ExistEnumType_When_Handling_Then_ShouldReturnDictionaryOfEnumtItems(
+            [Frozen] Mock<IReadGenericRepository<EnumType>> enumTypeRepository,
+            EnumItemQuery query,
+            EnumItemQueryHandler handler,
+            EnumType enumType)
+        {
+            // Arrange
+            List<EnumItemResult> enumItemResults = enumType.EnumTypeItems.Select(eti => Mapper.Map<EnumItemResult>(eti)).ToList();
+            enumTypeRepository.Setup(r => r.GetWithInclude(et => et.EnumTypeItems)).Returns(new[] { enumType }.AsQueryable());
+
+            // Act
+            Dictionary<string, ICollection<EnumItemResult>> dictionary = handler.Handle(query);
+
+            // Assert
+            dictionary.Should().NotBeNull();
+            dictionary.Should().HaveCount(1);
+            dictionary.Should().ContainKey(enumType.Code);
+            dictionary[enumType.Code].Should().NotBeNull();
+            dictionary[enumType.Code].Should().HaveCount(enumType.EnumTypeItems.Count);
+            dictionary[enumType.Code].ShouldAllBeEquivalentTo(enumItemResults);
+        }
+    }
+}
