c311b59895fd63586767b2904dd8024f379f726e
Bartosz Szatanik <bszatanik@objectivity.co.uk>
2016-04-29 13:13:38 +0200

    # 24214 #24215 Fix after merge Reviewed by: Ryszard Seniuta

--- src/Tests/KnightFrank.Antares.Domain.UnitTests/Viewing/CommandHandlers/CreateViewingCommandHandlerTests.cs
+++ src/Tests/KnightFrank.Antares.Domain.UnitTests/Viewing/CommandHandlers/CreateViewingCommandHandlerTests.cs
@@ -79,6 +79,8 @@
         {
             //Arrange
             this.requirementRepository.Setup(r => r.GetById(It.IsAny<Guid>())).Returns((Requirement)null);
+            this.entityValidator.Setup(x => x.EntityExits(It.IsAny<Requirement>(),It.IsAny<Guid>()))
+                .Throws(new BusinessValidationException(It.IsAny<BusinessValidationMessage>()));
 
             // Act + Assert
             Assert.Throws<BusinessValidationException>(() => { this.handler.Handle(this.command); });
@@ -89,7 +91,7 @@
         public void Given_CreateViewingCommandWithInvalidActivityId_When_Handle_Then_ShouldThrowBusinessException()
         {
             //Arrange
-            this.entityValidator.Setup(x => x.ThrowExceptionIfNotExist<Activity>(It.IsAny<Guid>()))
+            this.entityValidator.Setup(x => x.EntityExits<Activity>(It.IsAny<Guid>()))
                 .Throws(new BusinessValidationException(It.IsAny<BusinessValidationMessage>()));
 
             // Act + Assert
