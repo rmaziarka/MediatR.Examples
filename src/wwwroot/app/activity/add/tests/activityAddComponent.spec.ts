a511ff3918429322f93238804861e38fb3054c00
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-05-04 11:08:16 +0200

    #24372 Use enums component for activity types on activity add component + jasmin tests Review by: Zbyszek Rubiński

--- src/wwwroot/app/activity/add/tests/activityAddComponent.spec.ts
+++ src/wwwroot/app/activity/add/tests/activityAddComponent.spec.ts
@@ -5,6 +5,8 @@ module Antares {
     import Dto = Common.Models.Dto;
     import Business = Common.Models.Business;
 
+    import runDescribe = TestHelpers.runDescribe;
+
     describe('Given activity is being added', () => {
         var scope: ng.IScope,
             element: ng.IAugmentedJQuery,
@@ -14,7 +16,7 @@ module Antares {
             controller: ActivityAddController;
 
         var pageObjectSelectors = {
-            activityStatusSelector: 'select#status',
+            activityStatusSelector: 'enum-select#status select',
             activityTypeSelector: 'select#type',
             vendors: '#activity-add-vendors [id^=list-item-]'
         };
@@ -26,14 +28,11 @@ module Antares {
                 { id: "3", order: 3 }
             ];
 
-        var activityStatuses: any = {
-            items:
-            [
-                { id: "1", value: "Pre-appraisal", code: "PreAppraisal" },
-                { id: "2", value: "Market appraisal", code: "MarketAppraisal" },
-                { id: "3", value: "Not selling", code: "NotSelling" }
-            ]
-        };
+        var activityStatuses = [
+            { id: "111", code: "PreAppraisal" },
+            { id: "testStatus222", code: "MarketAppraisal" },
+            { id: "333", code: "NotSelling" }
+        ];
 
         var createVendors = (count: number) => {
             return _.map(TestHelpers.ContactGenerator.generateMany(count), (dtoContact: Dto.IContact) => {
@@ -44,18 +43,18 @@ module Antares {
         beforeEach(inject((
             $rootScope: ng.IRootScopeService,
             $compile: ng.ICompileService,
+            enumService: Mock.EnumServiceMock,
             $httpBackend: ng.IHttpBackendService) => {
 
             $http = $httpBackend;
             compile = $compile;
 
-            $http.expectGET(/\/api\/enums\/ActivityStatus\/items/).respond(() => {
-                return [200, activityStatuses];
-            });
-
             $http.expectGET(/\/api\/activities\/types/).respond(() => {
                 return [200, activityTypes];
             });
+
+            // enums
+            enumService.setEnum(Dto.EnumTypeCode.ActivityStatus.toString(), activityStatuses);
 
             scope = $rootScope.$new();
             element = compile('<activity-add></activity-add>')(scope);
@@ -70,23 +69,8 @@ module Antares {
 
         describe('when component is being loaded', () => {
             it('then default Pre-appraisal status is selected', () => {
-                var expectedPreAppraisalStatus = _.find(activityStatuses.items, { 'code': 'PreAppraisal' });
-                expect(controller.selectedActivityStatus).toEqual(expectedPreAppraisalStatus);
-            });
-        });
-
-        describe('when vendors are passed to component setVendors method', () => {
-            it('and are null then empty vendors array should be set ', () => {
-                controller.setVendors(null);
-
-                expect(controller.vendors.length).toBe(0);
-            });
-
-            it('and are array then vendors array should be set ', () => {
-                var vendorsArray = createVendors(3);
-                controller.setVendors(vendorsArray);
-
-                expect(controller.vendors).toBe(vendorsArray);
+                var expectedPreAppraisalStatus = _.find(activityStatuses, { 'code': 'PreAppraisal' });
+                expect(controller.selectedActivityStatusId).toEqual(expectedPreAppraisalStatus.id);
             });
         });
 
@@ -120,114 +104,33 @@ module Antares {
             });
         });
 
-        describe('when activity status value is ', () => {
-            it('missing then required message should be displayed', () => {
-                assertValidator.assertRequiredValidator(null, false, pageObjectSelectors.activityStatusSelector);
+        type TestCaseForRequiredValidator = [string, boolean];
+        // RequiredValidator for activity status
+        runDescribe('when activity status ')
+            .data<TestCaseForRequiredValidator>([
+                [activityStatuses[2].id, true],
+                ['statusIdThatIsNotOnTheList', false],
+                ['', false],
+                [null, false]])
+            .dataIt((data: TestCaseForRequiredValidator) =>
+                `value is "${data[0]}" then required message should ${data[1] ? 'not' : ''} be displayed`)
+            .run((data: TestCaseForRequiredValidator) => {
+                // arrange / act / assert
+                assertValidator.assertRequiredValidator(data[0], data[1], pageObjectSelectors.activityStatusSelector);
             });
 
-            it('not missing then required message should not be displayed', () => {
-                assertValidator.assertRequiredValidator('2', true, pageObjectSelectors.activityStatusSelector);
+        // RequiredValidator for activity type
+        runDescribe('when activity status ')
+            .data<TestCaseForRequiredValidator>([
+                [activityTypes[1].id, true],
+                ['typeIdThatIsNotOnTheList', false],
+                ['', false],
+                [null, false]])
+            .dataIt((data: TestCaseForRequiredValidator) =>
+                `value is "${data[0]}" then required message should ${data[1] ? 'not' : ''} be displayed`)
+            .run((data: TestCaseForRequiredValidator) => {
+                // arrange / act / assert
+                assertValidator.assertRequiredValidator(data[0], data[1], pageObjectSelectors.activityTypeSelector);
             });
-        });
-
-        describe('when activity type value is ', () => {
-            it('missing then required message should be displayed', () => {
-                assertValidator.assertRequiredValidator(null, false, pageObjectSelectors.activityTypeSelector);
-            });
-
-            it('not missing then required message should not be displayed', () => {
-                assertValidator.assertRequiredValidator('2', true, pageObjectSelectors.activityTypeSelector);
-            });
-        });
-
-        describe('when "Save button" is clicked ', () => {
-            var propertyMock: Dto.IProperty;
-
-            beforeEach(inject((
-                $rootScope: ng.IRootScopeService,
-                $compile: ng.ICompileService,
-                $httpBackend: ng.IHttpBackendService) => {
-
-                $http = $httpBackend;
-
-                $httpBackend.whenGET("").respond(() => { return {}; });
-
-                scope = $rootScope.$new();
-                propertyMock = TestHelpers.PropertyGenerator.generateDto();
-                scope['property'] = propertyMock;
-                element = $compile('<property-view property="property"></property-view>')(scope);
-
-                scope.$apply();
-                $http.flush();
-            }
-            ));
-
-            it('then proper data should be send to API', () => {
-                var vendors = createVendors(4);
-
-                var requestData: Business.CreateActivityResource;
-
-                var activityAddController: Activity.ActivityAddController = element.find('activity-add').controller('activityAdd');
-                activityAddController.activityStatuses = activityStatuses.items;
-                activityAddController.activityTypes = activityTypes;
-                activityAddController.selectedActivityStatus = _.find(activityStatuses.items, { 'code': 'PreAppraisal' });
-                activityAddController.selectedActivityType = _.find(activityTypes, { id: "1" });
-                activityAddController.setVendors(vendors);
-
-                var expectedRequest = new Business.CreateActivityResource();
-                expectedRequest.propertyId = propertyMock.id;
-                expectedRequest.activityStatusId = activityAddController.selectedActivityStatus.id;
-                expectedRequest.activityTypeId = activityAddController.selectedActivityType.id;
-                expectedRequest.contactIds = vendors.map((vendor: Dto.IContact) => { return vendor.id });
-
-                scope.$apply();
-
-                $http.expectPOST(/\/api\/activities/, (data: string) => {
-                    requestData = JSON.parse(data);
-
-                    return true;
-                }).respond(201, new Business.Activity());
-
-                element.find('#activity-add-button').click();
-                $http.flush();
-
-                expect(requestData.activityStatusId).toEqual(expectedRequest.activityStatusId);
-                expect(requestData.propertyId).toEqual(expectedRequest.propertyId);
-                expect(requestData.contactIds.length).toEqual(expectedRequest.contactIds.length);
-                expect(angular.equals(requestData.contactIds, expectedRequest.contactIds)).toBe(true);
-            });
-
-            it('then new activity should be added to property activity list', () => {
-                var vendors = createVendors(2);
-
-                var activityAddController: Activity.ActivityAddController = element.find('activity-add').controller('activityAdd');
-                activityAddController.activityStatuses = activityStatuses.items;
-                activityAddController.activityTypes = activityTypes;
-                activityAddController.selectedActivityStatus = _.find(activityStatuses.items, { 'code': 'PreAppraisal' });
-                activityAddController.selectedActivityType = _.find(activityTypes, { id: "1" });
-                activityAddController.setVendors(vendors);
-
-                var expectedResponse = new Business.Activity();
-                expectedResponse.propertyId = propertyMock.id;
-                expectedResponse.activityStatusId = activityAddController.selectedActivityStatus.id;
-                expectedResponse.activityTypeId = activityAddController.selectedActivityType.id;
-                expectedResponse.contacts = vendors;
-                expectedResponse.createdDate = new Date('2016-02-03');
-                expectedResponse.id = '123';
-
-                scope.$apply();
-
-                $http.whenPOST(/\/api\/activities/).respond(201, expectedResponse);
-
-                element.find('#activity-add-button').click();
-                $http.flush();
-
-                var activitiesList = element.find('#card-list-activities');
-                var activityListItems = activitiesList.find('card');
-
-                expect(propertyMock.activities.filter((item) => { return item.id === '123' }).length).toBe(1);
-                expect(activityListItems.length).toBe(1);
-            });
-        });
     });
 }
