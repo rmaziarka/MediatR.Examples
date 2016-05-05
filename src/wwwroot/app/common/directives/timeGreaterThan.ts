4f13e19c7bc6c366d60c20aa039f29ad92729c92
Bartosz Szatanik <bszatanik@objectivity.co.uk>
2016-05-05 13:55:26 +0200

    [#22717][#23408] Add time validation. Add two new validatiors timeLowerThan, timeGreaterThan. Reviewed by: Karolina Czerniawska

---
+++ src/wwwroot/app/common/directives/timeGreaterThan.ts
@@ -0,0 +1,43 @@
+/// <reference path="../../typings/_all.d.ts" />
+namespace Antares.Common.Directive {
+    
+    declare var moment:any;
+    class TimeGreaterThan {
+        require: string = 'ngModel';
+
+        constructor() {
+        }
+
+        link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, ctrl: ng.IControllerService) => {
+            var ngModel: ng.INgModelController = element.controller('ngModel');
+            var validateDateRange = (inputValue: string): string => {
+                if (!inputValue || !attrs['timeGreaterThan']) {
+                    ngModel.$setValidity('timeGreaterThan', true);
+                    return inputValue;
+                }
+
+                var fromDate: Date = moment(attrs['timeGreaterThan'].replace(/\"/g,""));
+                var toDate: Date = moment(inputValue);
+                var isValid: boolean = moment(fromDate).isSameOrBefore(toDate);
+                ngModel.$setValidity('timeGreaterThan', isValid);
+                return inputValue;
+            };
+
+            ngModel.$parsers.push(validateDateRange);
+            ngModel.$formatters.push(validateDateRange);
+            attrs.$observe('timeGreaterThan', () => {
+                validateDateRange(ngModel.$viewValue);
+            });
+        }
+        static factory() {
+            var directive = (): TimeGreaterThan => {
+                return new TimeGreaterThan();
+            };
+
+            directive['$inject'] = [];
+            return directive;
+        }
+    }
+
+    angular.module('app').directive('timeGreaterThan', TimeGreaterThan.factory());
+}
---
+++ src/wwwroot/app/common/directives/timeLowerThan.ts
@@ -0,0 +1,42 @@
+/// <reference path="../../typings/_all.d.ts" />
+namespace Antares.Common.Directive {
+    declare var moment:any;
+    class TimeLowerThan {
+        constructor() {
+        }
+
+        require = 'ngModel';
+        link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, ctrl: ng.IControllerService) => {
+            var ngModel: ng.INgModelController = element.controller('ngModel');
+            var validateDateRange = (inputValue: string) => {
+
+                if (!inputValue || !attrs['timeLowerThan']) {
+                    ngModel.$setValidity('timeLowerThan', true);
+                    return inputValue;
+                }
+                var fromDate = moment(inputValue);
+                var toDate = moment(attrs['timeLowerThan'].replace(/\"/g,""));
+                var isValid = moment(fromDate).isSameOrBefore(toDate,'minute');
+                ngModel.$setValidity('timeLowerThan', isValid);
+                return inputValue;
+            };
+
+            ngModel.$parsers.push(validateDateRange);
+            ngModel.$formatters.push(validateDateRange);
+            attrs.$observe('timeLowerThan', function () {
+                validateDateRange(ngModel.$viewValue);
+            });
+        }
+
+        static factory() {
+            var directive = () => {
+                return new TimeLowerThan();
+            };
+
+            directive['$inject'] = [];
+            return directive;
+        }
+    }
+
+    angular.module('app').directive('timeLowerThan', TimeLowerThan.factory());
+}
