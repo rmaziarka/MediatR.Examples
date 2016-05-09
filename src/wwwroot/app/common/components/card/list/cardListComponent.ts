78aca72a35cdc2ba741af3cddb1f21b4377709d4
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-05-09 11:36:10 +0200

    #20480 #24760 Fix bug - blocking "add" attachment activity while uploading Reviewed by: Marcin Biernacik

--- src/wwwroot/app/common/components/card/list/cardListComponent.ts
+++ src/wwwroot/app/common/components/card/list/cardListComponent.ts
@@ -11,7 +11,8 @@ module Antares.Common.Component {
             'item': '?cardListItem'
         },
         bindings: {
-            showItemAdd: '<'
+            showItemAdd: '<',
+            showItemAddDisabled: '<'
         }
     });
 }
