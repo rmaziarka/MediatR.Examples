31f8196db957042abeb79f75af19611ed797e051
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-05-10 12:53:17 +0200

    #20481 #24821 FileSize filter Reviewed by: Marcin Biernacik

---
+++ src/wwwroot/app/common/filters/fileSize/fileSizeFilter.ts
@@ -0,0 +1,24 @@
+﻿/// <reference path="../../../typings/_all.d.ts" />
+
+module Antares.Common.Filters {
+
+    declare var filesize: Filesize.IFilesize;
+
+    export class FileSizeFilter {
+        constructor(private filesize: Filesize.IFilesize) {
+        }
+
+        transform = (value: number): string => {
+            return angular.isNumber(value) ? this.filesize(value, { round: 1 }) : null;
+        };
+
+        static getInstance() {
+            return (value: number) => {
+                var filter = new FileSizeFilter(filesize);
+                return filter.transform(value);
+            };
+        }
+    }
+
+    angular.module('app').filter('fileSize', FileSizeFilter.getInstance);
+}
