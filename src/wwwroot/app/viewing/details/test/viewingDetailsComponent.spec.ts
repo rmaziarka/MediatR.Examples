31beab453618633d9cdfb293ae57a217cf986c5f
Ryszard Seniuta <rseniuta@objectivity.co.uk>
2016-05-10 12:52:53 +0200

    #23464 Viewing add unit tests
    Reviewed by Lukasz Misztal

--- src/wwwroot/app/viewing/details/test/viewingDetailsComponent.spec.ts
+++ src/wwwroot/app/viewing/details/test/viewingDetailsComponent.spec.ts
@@ -3,15 +3,14 @@
 module Antares {
     import ViewingDetailsController = Antares.Component.ViewingDetailsController;
     import Dto = Common.Models.Dto;
-
+    import Business = Common.Models.Business;
     import runDescribe = TestHelpers.runDescribe;
 
     describe('Given viewing is being added', () => {
         var scope: ng.IScope,
             element: ng.IAugmentedJQuery,
             assertValidator: Antares.TestHelpers.AssertValidators,
-            $http: ng.IHttpBackendService,
-            $compileService: ng.ICompileService;
+            $http: ng.IHttpBackendService;
 
         var pageObjectSelectors = {
             viewingDateSelector: '[name=viewingStartDate]',
@@ -21,18 +20,34 @@ module Antares {
             endTimeInputSelector: '[name=endTime] input',
             invitationTextSelector: '#invitation-text',
             postViewingComment: '#post-viewing-comment',
-            atendeesList: 'list-not-empty',
+            atendeesListValidator: 'list-not-empty',
             requiredValidatorSelector: '[name="requiredValidationError"]',
             timeValidatorSelector: '[name="timeValidationError"]',
             listNotEmptyValidatorSelector: '[name="listNotEmptyValidationError"]',
             timeLowerValidatorSelector: '[name = "timeLowerThanValidationError"]',
-            timeGreaterValidatorSelector: '[name = "timeGreaterThanValidationError"]'
+            timeGreaterValidatorSelector: '[name = "timeGreaterThanValidationError"]',
+            attendesElementsSelector: '#attendees-list input:checkbox',
+            viewingActivitySelector: '#viewing-activity'
         };
         
         var pageObject = {
             inputValidCss: 'ng-valid'
         };
 
+        var activityMock: Business.ActivityQueryResult = {
+            id: "id",
+            propertyName: "propertyName",
+            propertyNumber: "propertyNumber",
+            line2: "line2"
+        };
+
+        var requirementMock: Business.Requirement = TestHelpers.RequirementGenerator.generate({
+            contacts: [
+                { id: '1', firstName: 'John', surname: 'Doe', title: 'Mr.' },
+                { id: '2', firstName: 'Jane', surname: 'Doe', title: 'Mrs.' }
+            ]
+        });
+        
         var controller: ViewingDetailsController;
 
         beforeEach(inject((
@@ -41,15 +56,17 @@ module Antares {
             $httpBackend: ng.IHttpBackendService) => {
 
             $http = $httpBackend;
-            $compileService = $compile;
-            scope = $rootScope.$new();
-            scope["attendees"] = [];
-            element = $compile('<viewing-details attendees="vm.attendees"></viewing-details>')(scope);
+            scope = $rootScope.$new();           
+            scope["requirement"] = requirementMock;
+            scope["attendees"] = requirementMock.contacts;
+            
+            element = $compile('<viewing-details requirement="requirement" attendees="attendees"></viewing-details>')(scope);                        
             scope.$apply();
 
             controller = element.controller('viewingDetails');
             controller.startTime = "10:00";
             controller.endTime = "11:00";
+            
             assertValidator = new Antares.TestHelpers.AssertValidators(element, scope);
         }));
 
@@ -92,7 +109,7 @@ module Antares {
                 expect(wrapper.parent().find(pageObjectSelectors.requiredValidatorSelector).length).toBe(1);
             });
 
-            xit('end time is lesser than start time then validation message should be displayed', () => {
+            it('end time is lesser than start time then validation message should be displayed', () => {
                 // arrange
                 var startTimewrapper = element.find(pageObjectSelectors.startTimeSelector);
                 var endTimewrapper = element.find(pageObjectSelectors.endTimeSelector);
@@ -105,17 +122,38 @@ module Antares {
                 expect(startTimewrapper.hasClass(pageObject.inputValidCss)).toBe(false);
                 expect(endTimewrapper.hasClass(pageObject.inputValidCss)).toBe(false);
                 expect(startTimewrapper.parent().find(pageObjectSelectors.timeLowerValidatorSelector).length).toBe(1);
-                expect(endTimewrapper.parent().find(pageObjectSelectors.timeLowerValidatorSelector).length).toBe(1);
+                expect(endTimewrapper.parent().find(pageObjectSelectors.timeGreaterValidatorSelector).length).toBe(1);
             });
 
             it('no atendees are selected then validation message should be displayed', () => {
                 // arrange
-                var list = element.find(pageObjectSelectors.atendeesList);
+                var listValidator = element.find(pageObjectSelectors.atendeesListValidator);
                 // act
                 var form = element.find('form').triggerHandler('submit');
                 // assert
+                expect(listValidator.hasClass(pageObject.inputValidCss)).toBe(false);
+                expect(element.find(pageObjectSelectors.listNotEmptyValidatorSelector).length).toBe(1);
+            });
+
+            it('atendees are selected and unselected then validation message should be displayed', () => {
+                // arrange
+                var list = element.find(pageObjectSelectors.atendeesListValidator);
+                // act
+                element.find(pageObjectSelectors.attendesElementsSelector).first().click();
+                element.find(pageObjectSelectors.attendesElementsSelector).first().click();
+                // assert
                 expect(list.hasClass(pageObject.inputValidCss)).toBe(false);
                 expect(element.find(pageObjectSelectors.listNotEmptyValidatorSelector).length).toBe(1);
+            });
+
+            it('selected activity is displayed', () => {
+                // arrange
+                var activityElement = element.find(pageObjectSelectors.viewingActivitySelector); 
+                // act
+                controller.activity = activityMock;
+                scope.$apply();               
+                // assert
+                expect(activityElement.text()).toContain(activityMock.propertyName);
             });
 
             runDescribe('start time')
