6be7bde5bddd37b504b02861b3a5087dd7542f7f
Michal Lenartowicz <mlenartowicz@objectivity.co.uk>
2016-05-12 16:06:25 +0200

    #25283 Unnecessary validation message after clicking save button while editing viewing Reviewed by: Bartosz Szatanik

--- src/wwwroot/app/common/models/business/viewing/viewing.ts
+++ src/wwwroot/app/common/models/business/viewing/viewing.ts
@@ -1,6 +1,8 @@
 ﻿/// <reference path="../../../../typings/_all.d.ts" />
 
 module Antares.Common.Models.Business {
+    declare var moment: any;
+
     export class Viewing implements Dto.IViewing {
         id: string = null;
         activityId: string = null;
@@ -15,13 +17,14 @@ module Antares.Common.Models.Business {
         attendees: Contact[];
         day: string;
         activity: Dto.IActivity;
-        requirement: Dto.IRequirement;   
+        requirement: Dto.IRequirement;
 
         constructor(viewing?: Dto.IViewing) {
             if (viewing) {
                 angular.extend(this, viewing);
                 this.day = Core.DateTimeUtils.getDatePart(viewing.startDate);
                 this.requirement = new Requirement(viewing.requirement);
+                this.startDate = moment(viewing.startDate).toDate(); 
             }
         }
     }
