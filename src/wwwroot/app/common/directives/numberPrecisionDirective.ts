8f0882c339b895caa9058f4dc88b5731bde901d6
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-05-19 12:51:14 +0200

    #22718  #25637 number precision directive; allow only integer offer price
    Reviewed by: Bartosz Szatanik

---
+++ src/wwwroot/app/common/directives/numberPrecisionDirective.ts
@@ -0,0 +1,40 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.Common.Directive {
+    export class NumberPrecisionDirective implements ng.IDirective {
+        restrict = 'A';
+        require = 'ngModel';
+        scope = {
+            ngModel: '=ngModel',
+            numberPrecision: '='
+        };
+        link: any;
+
+        constructor() {
+            this.link = this.unboundLink.bind(this);
+        }
+
+        unboundLink(scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController) {
+            ngModel.$validators['numberPrecision'] = (modelValue: string) => {
+                if (!modelValue) {
+                    return true;
+                }
+                
+                var precision: number = parseInt(<string>scope["numberPrecision"]);
+                var floatRegEx: RegExp = precision > 0 ? new RegExp("^\\-?\\d+([\\.\\,]\\d{1," + precision + "})?$"): /^\-?\d+$/;
+                return floatRegEx.test(modelValue);
+            }
+        }
+
+        static factory() {
+            var directive = () => {
+                return new NumberPrecisionDirective();
+            };
+
+            directive['$inject'] = [];
+            return directive;
+        }
+    }
+
+    angular.module('app').directive('numberPrecision', NumberPrecisionDirective.factory());
+}
