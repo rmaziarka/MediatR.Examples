264b4ce7f75737a73b42ff46d0ba0c76a69871f1
Michal Jankowski <mjankowski@objectivity.co.uk>
2016-06-02 10:47:11 +0200

    #23902 #26086 Component skeleton Reviewed by: Marta Dycjan

---
+++ src/wwwroot/app/common/components/editableDate/editableDateComponent.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../../../typings/_all.d.ts" />
+
+module Antares.Common.Component {
+    angular.module('app').component('editableDate', {
+        templateUrl: 'app/common/components/editableDate/editableDate.html',
+        controllerAs: 'vm',
+        controller: 'EditableDateController',
+        bindings: {
+            selectedDate: '='
+        }
+    });
+}
