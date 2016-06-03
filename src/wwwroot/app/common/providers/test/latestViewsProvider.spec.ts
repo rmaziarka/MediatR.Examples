50a2c81440c978008e32f2ac5d8a5b2313584357
Radek Maziarka <rmaziarka@objectivity.co.uk>
2016-06-03 15:26:01 +0200

    #25327 #26041 Add tests to latestViewsProvider Reviewed by Piotr Izak

---
+++ src/wwwroot/app/common/providers/test/latestViewsProvider.spec.ts
@@ -0,0 +1,185 @@
+﻿/// <reference path="../../../typings/_all.d.ts" />
+
+module Antares {
+    import LatestViewsService = Services.LatestViewsService;
+    import LatestViewsProvider = Providers.LatestViewsProvider;
+    import LatestViewCommand = Common.Models.Commands.ICreateLatestViewCommand;
+    import EntityTypeEnum = Common.Models.Enums.EntityTypeEnum;
+    import LatestViewGenerator = TestHelpers.LatestViewGenerator;
+    import LatestViewResultItem = Common.Models.Dto.ILatestViewResultItem;
+    import Address = Common.Models.Business.Address;
+
+    describe('latestViewsProvider', () => {
+        var
+            service: LatestViewsService,
+            provider: LatestViewsProvider,
+            q: ng.IQService,
+            rootScope: ng.IRootScopeService;
+
+        beforeEach(inject((
+            latestViewsService: LatestViewsService,
+            latestViewsProvider: LatestViewsProvider,
+            $q: ng.IQService,
+            $rootScope: ng.IRootScopeService) => {
+
+            service = latestViewsService;
+            provider = latestViewsProvider;
+            q = $q;
+            rootScope = $rootScope;
+        }));
+
+        describe('when refresh is called', () => {
+            var returnedValue: any = { test: 1 };
+
+            beforeEach(() =>{
+                // arrange
+                spyOn(service, 'get').and.callFake(() =>{
+                        var deffered = q.defer();
+                        deffered.resolve(returnedValue);
+                        return deffered.promise;
+                    });
+
+                spyOn(provider, 'loadLatestData');
+
+                // act 
+                provider.refresh();
+                rootScope.$digest();
+            });
+
+            it('then latestViewsService get method should be called', () =>{
+                expect(service.get).toHaveBeenCalled();
+            });
+
+            it('then loadLatestData method should be called with passed value from service', () => {
+                expect(provider.loadLatestData).toHaveBeenCalledWith(returnedValue);
+            });
+        });
+
+        describe('when addView is called with command', () =>{
+            var command: LatestViewCommand;
+            var returnedValue: any = { test: 1 };
+
+            beforeEach(() =>{
+                // arrange
+                command = { entityId: 'guid', entityType: EntityTypeEnum.Property };
+
+                spyOn(service, 'post').and.callFake(() =>{
+                        var deffered = q.defer();
+                        deffered.resolve(returnedValue);
+                        return deffered.promise;
+                    });
+
+                spyOn(provider, 'loadLatestData');
+
+                // act 
+                provider.addView(command);
+                rootScope.$digest();
+            });
+
+            it('then latestViewsService post method should be called with command', () =>{
+                expect(service.post).toHaveBeenCalledWith(command);
+            });
+
+            it('then loadLatestData method should be called with passed value from service', () => {
+                expect(provider.loadLatestData).toHaveBeenCalledWith(returnedValue);
+            });
+        });
+
+        describe('when loadLatestData is called with http result', () => {
+            var result: angular.IHttpPromiseCallbackArg<LatestViewResultItem[]> = {
+                data: []
+            };
+
+            beforeEach(() => {
+                spyOn(provider, 'loadProperties');
+                spyOn(provider, 'loadActivities');
+
+                // act 
+                provider.loadLatestData(result);
+            });
+
+            it('then loadProperties method should be called with http result data', () =>{
+                expect(provider.loadProperties).toHaveBeenCalledWith(result.data);
+            });
+
+            it('then loadActivities method should be called with http result data', () =>{
+                expect(provider.loadActivities).toHaveBeenCalledWith(result.data);
+            });
+        });
+
+        describe('when loadProperties is called with latest views', () => {
+
+            beforeEach(inject(($state: angular.ui.IState) => {
+                // arrange 
+                spyOn($state, 'href')
+                    .and.callFake((state: string, obj:any) =>{
+                        return state + obj.id;
+                    });
+            }));
+            
+            it('if there is no property views then properties field is undefined', () => {
+                //arrange
+                var views: LatestViewResultItem[] = [];
+
+                //act
+                provider.loadProperties(views);
+
+                //assert
+                expect(provider.properties).toBeUndefined();
+            });
+
+            it('if there are three property views then properties field should contains three entries', () => {
+                //arrange
+                var propertyViews = LatestViewGenerator.generatePropertyList(3);
+                var views = [
+                    propertyViews
+                ];
+
+                //act
+                provider.loadProperties(views);
+
+                //assert
+                expect(provider.properties.length).toBe(3);
+            });
+
+            it('if property address is fulfiled then entry should contain all data', () => {
+                //arrange
+                var propertyViews = LatestViewGenerator.generatePropertyList(1);
+                var views = [
+                    propertyViews
+                ];
+
+                //act
+                provider.loadProperties(views);
+
+                //assert
+                var propertyView = propertyViews.list[0];
+                var expectedId = propertyView.id;
+                var expectedAddressLine = new Address(propertyView.data).getAddressText();
+                var expectedUrl = "app.property-view" + expectedId;
+
+                var listProperty = provider.properties[0];
+                expect(listProperty.id).toBe(expectedId);
+                expect(listProperty.name).toBe(expectedAddressLine);
+                expect(listProperty.url).toBe(expectedUrl);
+            });
+
+            it('if property address is empty then entry should has dash as title', () => {
+                //arrange
+                var propertyViews = LatestViewGenerator.generatePropertyList(1);
+                var propertyView = propertyViews.list[0];
+                propertyView.data = new Address({});
+                var views = [
+                    propertyViews
+                ];
+
+                //act
+                provider.loadProperties(views);
+
+                //assert
+                var listProperty = provider.properties[0];
+                expect(listProperty.name).toBe('-');
+            });
+        });
+    });
+}
---
+++ src/wwwroot/app/common/testHelpers/latestViewGenerator.test.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.TestHelpers {
+    import Dto = Common.Models.Dto;
+    declare var moment: any;
+
+    export class LatestViewGenerator {
+        public static generatePropertyList(listLength: number): Dto.ILatestViewResultItem {
+            var item: Dto.ILatestViewResultItem = {
+                entityTypeCode: 'Property',
+                list: LatestViewGenerator.generateManyPropertyListItems(listLength)
+            };
+            
+            return item;
+        }
+
+        public static generatePropertyListItem(): Dto.ILatestViewData{
+            return {
+                id: LatestViewGenerator.makeRandom('id'),
+                createDate: moment().days(1),
+                data: Mock.AddressForm.FullAddress
+            }
+        }
+
+        public static generateManyPropertyListItems(n: number): Dto.ILatestViewData[] {
+            return _.times(n, LatestViewGenerator.generatePropertyListItem);
+        }
+
+        private static makeRandom(text: string): string {
+            return text + _.random(1, 1000000);
+        }
+    }
+}
