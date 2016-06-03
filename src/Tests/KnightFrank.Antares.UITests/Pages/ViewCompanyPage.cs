9437a6f7bf7535bdce5e31e0146c7d26c29754ee
Aneela Ahmed <aneela.ahmed@knightfrank.com>
2016-06-03 11:09:05 +0100

    #25349 #25943 updated selenium UI tests Reviewed By: Andrew Wakeling

---
+++ src/Tests/KnightFrank.Antares.UITests/Pages/ViewCompanyPage.cs
@@ -0,0 +1,55 @@
+﻿namespace KnightFrank.Antares.UITests.Pages
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Remoting.Messaging;
+
+    using KnightFrank.Antares.UITests.Pages.Panels;
+
+    using Objectivity.Test.Automation.Common;
+    using Objectivity.Test.Automation.Common.Extensions;
+    using Objectivity.Test.Automation.Common.Types;
+
+    using OpenQA.Selenium;
+
+    public class ViewCompanyPage : ProjectPageBase
+    {
+        private readonly ElementLocator companyViewForm = new ElementLocator(Locator.CssSelector, "company-view");
+        private readonly ElementLocator companyName = new ElementLocator(Locator.CssSelector, "div#name");
+        private readonly ElementLocator website = new ElementLocator(Locator.CssSelector ,"#websiteUrl a[name=url]");
+        private readonly ElementLocator clientCarePage = new ElementLocator(Locator.CssSelector, "#clientCarePageUrl a[name=url]");
+
+        private readonly ElementLocator clientCareStatus = new ElementLocator(Locator.Id, "client-care-status");
+        private readonly ElementLocator contactsList = new ElementLocator(Locator.CssSelector, "#list-contacts .ng-binding");
+        private readonly ElementLocator websiteUrlIcon = new ElementLocator(Locator.CssSelector, "a[name='url'] > i");
+
+       public ViewCompanyPage(DriverContext driverContext) : base(driverContext)
+        {
+        }
+
+        public ContactsListPage ContactsList => new ContactsListPage(this.DriverContext);
+
+        public List<string> Contacts => this.Driver.GetElements(this.contactsList).Select(el => el.Text).ToList();
+
+
+        public bool IsViewCompanyFormPresent()
+        {
+            return this.Driver.IsElementPresent(this.companyViewForm, BaseConfiguration.MediumTimeout);
+        }
+
+        public string GetCompanyName()
+        {
+            IWebElement element = this.Driver.GetElement(this.companyName);
+            return element.Text;
+        }
+    
+    public string GetWebsiteUrl()
+        {
+            return this.Driver.GetElement(this.website).Text;
+        }
+        public string GetClientCareUrl()
+        {
+            return this.Driver.GetElement(this.clientCarePage).Text;
+        }
+    }
+}
