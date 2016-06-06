4abce903a17fdf2db17bee3fade083ceaf4724f3
Patryk Kulecki <pkulecki@objectivity.co.uk>
2016-06-06 09:50:53 +0200

    #25332 #26072 integration tests for activities in latest views
    Reviewed by Radosław Maziarka

--- src/Tests/KnightFrank.Antares.Api.IntegrationTests/Steps/LatestViews/LatestViewsSteps.cs
+++ src/Tests/KnightFrank.Antares.Api.IntegrationTests/Steps/LatestViews/LatestViewsSteps.cs
@@ -14,6 +14,7 @@
     using KnightFrank.Antares.Dal.Model.Common;
     using KnightFrank.Antares.Dal.Model.LatestView;
     using KnightFrank.Antares.Dal.Model.Property;
+    using KnightFrank.Antares.Dal.Model.Property.Activities;
     using KnightFrank.Antares.Domain.LatestView.Commands;
     using KnightFrank.Antares.Domain.LatestView.QueryResults;
 
@@ -43,7 +44,7 @@
         }
 
         [Given(@"Property is added to latest views")]
-        public void AddToLatestViews()
+        public void AddPropertyToLatestViews()
         {
             Guid propertyId = this.scenarioContext.Get<Property>("AddedProperty").Id;
             List<Guid> propertiesIds = this.scenarioContext.ContainsKey("PropertiesIds")
@@ -65,15 +66,45 @@
             this.scenarioContext["PropertiesIds"] = propertiesIds;
         }
 
-        [When(@"User creates latest view")]
-        public void CreateLatestView()
+        [Given(@"Activity is added to latest views")]
+        public void AddActivityToLatestViews()
         {
-            Guid propertyId = this.scenarioContext.Get<Property>("AddedProperty").Id;
-            var details = new CreateLatestViewCommand
+            Guid activityId = this.scenarioContext.Get<Activity>("Activity").Id;
+            List<Guid> activitiesIds = this.scenarioContext.ContainsKey("ActivitiesIds")
+                ? this.scenarioContext.Get<List<Guid>>("ActivitiesIds")
+                : new List<Guid>();
+
+            activitiesIds.Add(activityId);
+
+            var details = new LatestView
             {
-                EntityType = EntityTypeEnum.Property,
-                EntityId = propertyId
+                CreatedDate = DateTime.UtcNow,
+                UserId = this.fixture.DataContext.Users.First().Id,
+                EntityId = activityId,
+                EntityType = EntityTypeEnum.Activity
             };
+
+            this.fixture.DataContext.LatestView.Add(details);
+            this.fixture.DataContext.SaveChanges();
+            this.scenarioContext["ActivitiesIds"] = activitiesIds;
+        }
+
+        [When(@"User adds (.*) to latest viewed entities using api")]
+        public void CreateLatestView(string entity)
+        {
+            var details = new CreateLatestViewCommand();
+            if (entity.ToLower().Equals("property"))
+            {
+                Guid propertyId = this.scenarioContext.Get<Property>("AddedProperty").Id;
+                details.EntityId = propertyId;
+                details.EntityType = EntityTypeEnum.Property;
+            }
+            else if (entity.ToLower().Equals("activity"))
+            {
+                Guid activityId = this.scenarioContext.Get<Activity>("Activity").Id;
+                details.EntityId = activityId;
+                details.EntityType = EntityTypeEnum.Activity;
+            }
 
             this.CreateLatestView(details);
         }
@@ -96,22 +127,42 @@
             this.GetLatestViews();
         }
 
-        [Then(@"Latest viewed properties details should match viewed properties")]
-        public void CheckLatestsViewedProperties()
+        [Then(@"Latest viewed details should match (.*) entities")]
+        public void CheckLatestsViewedProperties(string entity)
         {
-            List<Guid> propertiesIds = this.scenarioContext.Get<List<Guid>>("PropertiesIds").ToList();
-            propertiesIds.Reverse();
-            propertiesIds = propertiesIds.Distinct().Take(this.maxLatestViews).ToList();
+            List<Guid> entitiesIds;
+            var addressesIds = new List<Guid>();
+            string entityTypeCode = string.Empty;
 
-            List<Guid> addressesIds =
-                propertiesIds.Select(id => this.fixture.DataContext.Properties.Single(p => p.Id.Equals(id)).AddressId).ToList();
+            if (entity.ToLower().Equals("property"))
+            {
+                entitiesIds = this.scenarioContext.Get<List<Guid>>("PropertiesIds").ToList();
+                entitiesIds.Reverse();
+                entitiesIds = entitiesIds.Distinct().Take(this.maxLatestViews).ToList();
+                addressesIds =
+                    entitiesIds.Select(id => this.fixture.DataContext.Properties.Single(p => p.Id.Equals(id)).AddressId).ToList();
+                entityTypeCode = "Property";
+            }
+            else if (entity.ToLower().Equals("activity"))
+            {
+                entitiesIds = this.scenarioContext.Get<List<Guid>>("ActivitiesIds").ToList();
+                entitiesIds.Reverse();
+                entitiesIds = entitiesIds.Distinct().Take(this.maxLatestViews).ToList();
+                addressesIds =
+                    entitiesIds.Select(id => this.fixture.DataContext.Activities.Single(p => p.Id.Equals(id)).Property.AddressId)
+                               .ToList();
+                entityTypeCode = "Activity";
+            }
+
             List<Address> expectedAddresses =
                 addressesIds.Select(id => this.fixture.DataContext.Addresses.Single(a => a.Id.Equals(id))).ToList();
+            LatestViewQueryResultItem response =
+                JsonConvert.DeserializeObject<List<LatestViewQueryResultItem>>(this.scenarioContext.GetResponseContent()).Single();
 
-            List<LatestViewData> response =
-                JsonConvert.DeserializeObject<List<LatestViewQueryResultItem>>(this.scenarioContext.GetResponseContent())[0].List.ToList();
-            List<Address> currentAddresses =
-                response.Select(data => JsonConvert.DeserializeObject<Address>(data.Data.ToString())).ToList();
+            response.EntityTypeCode.Should().Be(entityTypeCode);
+
+            List<LatestViewData> data = response.List.ToList();
+            List<Address> currentAddresses = data.Select(d => JsonConvert.DeserializeObject<Address>(d.Data.ToString())).ToList();
 
             expectedAddresses.ShouldAllBeEquivalentTo(currentAddresses, opt => opt
                 .Excluding(a => a.Country));
@@ -120,29 +171,46 @@
                                                .Select(gLv => gLv.OrderByDescending(lv => lv.CreatedDate).FirstOrDefault())
                                                .OrderByDescending(lv => lv.CreatedDate).Take(this.maxLatestViews).ToList();
 
-            latestViews.Should().Equal(response, (v1, v2) => v1.CreatedDate.Equals(v2.CreatedDate));
+            latestViews.Should().Equal(data, (v1, v2) => v1.CreatedDate.Equals(v2.CreatedDate));
         }
 
-        [Then(@"Retrieved latest view should contain property")]
-        public void CheckLatestsViewedProperty()
+        [Then(@"Retrieved latest view should contain (.*) entity")]
+        public void CheckLatestsViewedEntity(string entity)
         {
-            Guid propertyId = this.scenarioContext.Get<Property>("AddedProperty").Id;
+            Guid entityId;
+            Guid addressId = Guid.NewGuid();
+            string entityTypeCode = string.Empty;
 
-            List<LatestViewData> response =
-                JsonConvert.DeserializeObject<List<LatestViewQueryResultItem>>(this.scenarioContext.GetResponseContent())[0].List.ToList();
+            if (entity.ToLower().Equals("property"))
+            {
+                entityId = this.scenarioContext.Get<Property>("AddedProperty").Id;
+                addressId = this.fixture.DataContext.Properties.Single(p => p.Id.Equals(entityId)).AddressId;
+                entityTypeCode = "Property";
+            }
+            else if (entity.ToLower().Equals("activity"))
+            {
+                entityId = this.scenarioContext.Get<Activity>("Activity").Id;
+                addressId = this.fixture.DataContext.Activities.Single(p => p.Id.Equals(entityId)).Property.AddressId;
+                entityTypeCode = "Activity";
+            }
 
-            response.Should().HaveCount(1);
+            LatestViewQueryResultItem response =
+                JsonConvert.DeserializeObject<List<LatestViewQueryResultItem>>(this.scenarioContext.GetResponseContent()).Single();
 
-            var currentAddress = JsonConvert.DeserializeObject<Address>(response[0].Data.ToString());
+            response.EntityTypeCode.Should().Be(entityTypeCode);
 
-            Guid addressId = this.fixture.DataContext.Properties.Single(p => p.Id.Equals(propertyId)).AddressId;
+            List<LatestViewData> data = response.List.ToList();
+            data.Should().HaveCount(1);
+
+            var currentAddress = JsonConvert.DeserializeObject<Address>(data.Single().Data.ToString());
             Address expectedAddress = this.fixture.DataContext.Addresses.Single(a => a.Id.Equals(addressId));
 
             expectedAddress.ShouldBeEquivalentTo(currentAddress, opt => opt.Excluding(a => a.Country));
 
-            LatestView latestView = this.fixture.DataContext.LatestView.First();
+            LatestView latestView = this.fixture.DataContext.LatestView.Single();
 
-            latestView.CreatedDate.Should().Be(response[0].CreatedDate);
+            latestView.CreatedDate.Should().Be(data.Single().CreatedDate);
+            latestView.EntityTypeString.Should().Be(entityTypeCode);
         }
 
         private void CreateLatestView(CreateLatestViewCommand command)
--- src/Tests/KnightFrank.Antares.Api.IntegrationTests/Tests/LatestViews/LatestViews.feature
+++ src/Tests/KnightFrank.Antares.Api.IntegrationTests/Tests/LatestViews/LatestViews.feature
@@ -1,7 +1,7 @@
 ﻿Feature: Latest views
 
 @LatestViews
-Scenario: Create latest view
+Scenario: Create latest viewed property
 	Given User gets GB address form for Property and country details
         And User gets EnumTypeItemId and EnumTypeItem code
 			| enumTypeCode | enumTypeItemCode |
@@ -10,9 +10,27 @@ Scenario: Create latest view
 		And Property with Address and Residential division is in database
 			| PropertyName | PropertyNumber | Line2 | Line3 | Postcode | City | County |
 			| 1            | 1              | 1     | 1     | 1        | 1    | 1      |
-	When User creates latest view
+	When User adds Property to latest viewed entities using api
 	Then User should get OK http status code
-		And Retrieved latest view should contain property
+		And Retrieved latest view should contain Property entity
+
+@LatestViews
+Scenario: Create latest viewed activity
+	Given User gets GB address form for Property and country details
+        And User gets EnumTypeItemId and EnumTypeItem code
+			| enumTypeCode     | enumTypeItemCode |
+			| Division         | Residential      |
+			| ActivityStatus   | PreAppraisal     |
+			| ActivityUserType | LeadNegotiator   |
+        And User gets House for PropertyType
+		And Property with Address and Residential division is in database
+			| PropertyName | PropertyNumber | Line2 | Line3 | Postcode | City | County |
+			| 1            | 1              | 1     | 1     | 1        | 1    | 1      |
+		And User gets Freehold Sale for ActivityType
+		And Activity for latest property and PreAppraisal activity status exists in database
+	When User adds Activity to latest viewed entities using api
+	Then User should get OK http status code
+		And Retrieved latest view should contain Activity entity
 
 @LatestViews
 Scenario: Create latest view using invalid entity type
@@ -41,4 +59,34 @@ Scenario: Get latest viewed properties
 		And Property is added to latest views
 	When User gets latest viewed entities
 	Then User should get OK http status code
-		And Latest viewed properties details should match viewed properties
+		And Latest viewed details should match Property entities
+
+@LatestViews
+Scenario: Get latest viewed activities
+	Given User gets GB address form for Property and country details
+        And User gets EnumTypeItemId and EnumTypeItem code
+			| enumTypeCode     | enumTypeItemCode |
+			| Division         | Residential      |
+			| ActivityStatus   | PreAppraisal     |
+			| ActivityUserType | LeadNegotiator   |
+        And User gets House for PropertyType
+		And User gets Freehold Sale for ActivityType
+		And Property with Address and Residential division is in database
+			| PropertyName | PropertyNumber | Line2 | Line3 | Postcode | City | County |
+			| 10           | 10             | 10    | 10    | 10       | 10   | 10     |
+		And Activity for latest property and PreAppraisal activity status exists in database
+		And Activity is added to latest views
+		And Property with Address and Residential division is in database
+			| PropertyName | PropertyNumber | Line2 | Line3 | Postcode | City | County |
+			| 20           | 20             | 20    | 20    | 20       | 20   | 20     |
+		And Activity for latest property and PreAppraisal activity status exists in database
+		And Activity is added to latest views
+		And Activity is added to latest views
+		And Property with Address and Residential division is in database
+			| PropertyName | PropertyNumber | Line2 | Line3 | Postcode | City | County |
+			| 30           | 30             | 30    | 30    | 30       | 30   | 30     |
+		And Activity for latest property and PreAppraisal activity status exists in database
+		And Activity is added to latest views
+	When User gets latest viewed entities
+	Then User should get OK http status code
+		And Latest viewed details should match Activity entities
