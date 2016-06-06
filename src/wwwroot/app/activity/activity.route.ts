c6cfc7be707ddebc545b0ee8dfcbe714cb90ff7c
Michal Lenartowicz <mlenartowicz@objectivity.co.uk>
2016-06-06 10:53:11 +0200

    #25332 Display recent activities in navigation drawer - unit tests Reviewed by: Pawel Zapalowski

--- src/wwwroot/app/activity/activity.route.ts
+++ src/wwwroot/app/activity/activity.route.ts
@@ -1,10 +1,6 @@
 /// <reference path="../typings/_all.d.ts" />
 
 module Antares.Activity {
-    import Dto = Common.Models.Dto;
-    import Business = Common.Models.Business;
-    import LatestViewsProvider = Providers.LatestViewsProvider;
-    import EntityType = Common.Models.Enums.EntityTypeEnum;
 
     var app: ng.IModule = angular.module('app');
     app.config(initRoute);
@@ -14,7 +10,7 @@ module Antares.Activity {
             .state('app.activity-view', {
                 url: '/activity/view/:id',
                 template: "<activity-view activity='activity'></activity-view>",
-                controller: ($scope: ng.IScope, activity: Dto.IActivity, latestViewsProvider: LatestViewsProvider) => new ActivityRouteController($scope, <Dto.IActivity>activity, <LatestViewsProvider>latestViewsProvider),
+                controller: "ActivityRouteController",
                 resolve: {
                     activity: ($stateParams: ng.ui.IStateParamsService, dataAccessService: Services.DataAccessService) => {
                         var activityId: string = $stateParams['id'];
@@ -25,7 +21,7 @@ module Antares.Activity {
             .state('app.activity-edit', {
                 url: '/activity/edit/:id',
                 template: "<activity-edit activity='activity'></activity-edit>",
-                controller: ($scope: ng.IScope, activity: Dto.IActivity, latestViewsProvider: LatestViewsProvider) => new ActivityRouteController($scope, <Dto.IActivity>activity, <LatestViewsProvider>latestViewsProvider),
+                controller: "ActivityRouteController",
                 resolve: {
                     activity: ($stateParams: ng.ui.IStateParamsService, dataAccessService: Services.DataAccessService) => {
                         var activityId: string = $stateParams['id'];
