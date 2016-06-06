654b3f573f26b43ead0fa0ab9a363bcc5103b742
Łukasz Misztal <lmisztal@objectivity.co.uk>
2016-06-06 11:58:54 +0200

    #25722 #26028 [JS] Add validation on offer form. Reviewed by: Michał Lenartowicz

--- src/wwwroot/app/offer/edit/offerEditController.ts
+++ src/wwwroot/app/offer/edit/offerEditController.ts
@@ -10,7 +10,6 @@ module Antares.Offer {
         selectedStatus: any;
         statuses: any;
         editOfferForm: any;
-        today: Date = new Date();
 
         offerDateOpen: boolean = false;
         exchangeDateOpen: boolean = false;
