d95800241edd687003a553e5c1ec136d798cb51e
Aneela Ahmed <aneela.ahmed@knightfrank.com>
2016-06-09 15:58:09 +0100

    #22726 #26730 added enums for salutaion format and API method Reviewed by: Andrew Wakeling

--- src/KnightFrank.Antares.Api/Controllers/UsersController.cs
+++ src/KnightFrank.Antares.Api/Controllers/UsersController.cs
@@ -1,57 +1,56 @@
 ﻿namespace KnightFrank.Antares.Api.Controllers
 {
+    using System;
     using System.Collections.Generic;
-    using System.Collections.Specialized;
-    using System.Linq;
-    using System.Security.Claims;
-    using System.Web.Configuration;
+    using System.Net;
+    using System.Net.Http;
     using System.Web.Http;
 
-    using KnightFrank.Antares.Dal.Model.Enum;
-    using KnightFrank.Antares.Dal.Repository;
     using KnightFrank.Antares.Domain.User.QueryResults;
 
     using Domain.User.Queries;
 
+    using KnightFrank.Antares.Dal.Model.User;
+
     using MediatR;
 
+    /// <summary>
+    /// UsersController
+    /// </summary>
     [RoutePrefix("api/users")]
     public class UsersController : ApiController
     {
-        private static readonly NameValueCollection config = WebConfigurationManager.AppSettings;
 
-        // TODO: here should inject repository (we should use handler)
-        private readonly IReadGenericRepository<EnumTypeItem> enumTypeItemRepository;
         private readonly IMediator mediator;
 
-        public UsersController(IReadGenericRepository<EnumTypeItem> enumTypeItemRepository, IMediator mediator)
+        /// <summary>
+        /// Initializes a new instance of the UsersController class.
+        /// </summary>
+        /// <param name="mediator">The mediator.</param>
+        public UsersController(IMediator mediator)
         {
-            this.enumTypeItemRepository = enumTypeItemRepository;
             this.mediator = mediator;
         }
-     
-        [Route("data")]
-        public UserDataResult GetUserData()
+
+
+        /// <summary>
+        /// Get User by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public User GetUserById(Guid id)
         {
-            var identity = (ClaimsIdentity)this.User.Identity;
+            User user = this.mediator.Send(new UserQuery { Id = id });
 
-            EnumTypeItem division = this.GetEnumTypeItemByCode(config["CurrentUser.Division"]);
-            var user = new UserDataResult
-                           {
-                               Name = identity.Name,
-                               Email = identity.Claims.First(c => c.Type == ClaimTypes.Email).Value,
-                               Country = identity.Claims.First(c => c.Type == ClaimTypes.Country).Value,
-                               Roles = identity.FindAll(ClaimTypes.Role).Select(claim => claim.Value),
-                               Division = division
-                           };
-
+            if (user == null)
+            {
+                throw new HttpResponseException(this.Request.CreateErrorResponse(HttpStatusCode.NotFound, "User not found."));
+            }
             return user;
         }
 
-        private EnumTypeItem GetEnumTypeItemByCode(string enumTypeItemCode)
-        {
-            return this.enumTypeItemRepository.Get().Single(eti => eti.Code == enumTypeItemCode);
-        }
 
         /// <summary>
         /// Get all users matching criteria within query
---
+++ src/KnightFrank.Antares.Domain/User/Queries/UserQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace KnightFrank.Antares.Domain.User.Queries
+{
+    using System;
+
+    using KnightFrank.Antares.Dal.Model.User;
+
+    using MediatR;
+
+    public class UserQuery : IRequest<User>
+    {
+        public Guid Id { get; set; }
+    }
+}
+
