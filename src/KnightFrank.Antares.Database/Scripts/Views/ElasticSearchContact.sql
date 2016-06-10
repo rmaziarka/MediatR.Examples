fc3abe92f5895c0e92b72f88cd7575e8339e9935
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2016-06-10 12:31:09 +0200

    #20403 #26830 Create sql json builders (View sql with join + flattening) #20403 #26831 Extend JDBC setting and prepare mapping json file for property and contact Reviewed by: Marcin Biernacik

---
+++ src/KnightFrank.Antares.Database/Scripts/Views/ElasticSearchContact.sql
@@ -0,0 +1,29 @@
+﻿IF EXISTS (SELECT TABLE_NAME 
+			FROM INFORMATION_SCHEMA.VIEWS
+			WHERE TABLE_NAME = 'ElasticSearchContact')
+	DROP VIEW [dbo].[ElasticSearchContact]
+GO
+
+CREATE VIEW [dbo].[ElasticSearchContact]
+AS
+	SELECT
+		-- ElasticSearch configuration
+		c.[Id] AS '_id',
+		'contact' AS '_type',
+				
+		-- Contact
+		c.Id AS 'Id',
+		c.FirstName AS 'FirstName',
+		c.Surname AS 'Surname',
+		c.Title AS 'Title',
+
+		-- CompanyContacts
+		cc.CompanyId AS 'CompanyContacts[CompanyId]',
+		cc.ContactId AS 'CompanyContacts[ContactId]',
+		comp.Id AS 'CompanyContacts[Company.Id]',
+		comp.Name AS 'CompanyContacts[Company.Name]'
+
+	FROM [dbo].[Contact] c
+	LEFT JOIN [dbo].[CompanyContact] cc ON cc.[ContactId] = c.[Id]
+	LEFT JOIN [dbo].[Company] comp ON comp.[Id] = cc.[CompanyId]
+GO
