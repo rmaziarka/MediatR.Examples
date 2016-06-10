d166fb8464de2ef7a9e2bded81c5d65a48e8039e
Ryszard Seniuta <rseniuta@objectivity.co.uk>
2016-06-10 08:48:07 +0200

    #26299 #26817 Offer details - added residential sales progress section
    Reviewed by Bartosz Szatanik

---
+++ src/wwwroot/app/common/filters/enumClassFilter/enumClassFilter.ts
@@ -0,0 +1,44 @@
+﻿/// <reference path="../../../typings/_all.d.ts" />
+
+module Antares {
+
+    export module Common {
+
+        export module Filters {
+
+	        export class EnumClassFilter {
+		        constructor(private enumService: Antares.Services.EnumService){
+		        }
+
+		        getEnumValue = (enumItemId: string) =>{
+			        return this.enumService.getEnumCodeById(enumItemId);
+		        }
+				
+		        camelToDash = (str: string) =>{
+					return str.replace(/\W+/g, '-')
+								.replace(/([a-z\d])([A-Z])/g, '$1-$2')
+								.toLowerCase();
+				}
+
+                static getInstance(enumService: Antares.Services.EnumService) {
+                    var mappingFunc: any = (id: string) =>{
+	                    var filter = new EnumClassFilter(enumService);
+	                    var code = filter.getEnumValue(id);
+	                    if (code !== undefined) {
+		                    return filter.camelToDash(code);
+	                    }
+	                    else {
+		                    return "";
+	                    }
+
+                    };
+                    mappingFunc.$stateful = true;
+
+                    return mappingFunc;
+                };
+            }
+
+            angular.module('app').filter('enumClass', EnumClassFilter.getInstance);
+        }
+    }
+}
