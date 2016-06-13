ce85f88cc30d4f23627e207f916b3ff1c100656c
Ryszard Seniuta <rseniuta@objectivity.co.uk>
2016-06-13 08:10:48 +0200

    #25185 #26966  Extend Offer to work with CompanyContact
    Reviewed by Lukasz Misztal

--- src/KnightFrank.Antares.Dal/Model/Configuration/Company/CompanyContactConfiguration.cs
+++ src/KnightFrank.Antares.Dal/Model/Configuration/Company/CompanyContactConfiguration.cs
@@ -1,18 +1,14 @@
 ﻿namespace KnightFrank.Antares.Dal.Model.Configuration.Company
 {
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Infrastructure.Annotations;
+
     using KnightFrank.Antares.Dal.Model.Company;
 
     internal sealed class CompanyContactConfiguration : BaseEntityConfiguration<CompanyContact>
     {
         public CompanyContactConfiguration()
         {
-            this.HasKey(q =>
-                new
-                {
-                    q.CompanyId,
-                    q.ContactId
-                });
-
             this.HasRequired(t => t.Contact)
                 .WithMany(t => t.CompaniesContacts)
                 .HasForeignKey(t => t.ContactId);
@@ -20,6 +16,13 @@
             this.HasRequired(t => t.Company)
                 .WithMany(t => t.CompaniesContacts)
                 .HasForeignKey(t => t.CompanyId);
+
+            var uniqueIndexName = "IX_CompanyId_ContactId";
+            this.Property(an => an.CompanyId)
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute(uniqueIndexName, 1) { IsUnique = true }));
+
+            this.Property(an => an.ContactId)
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute(uniqueIndexName, 2) { IsUnique = true }));
         }
     }
 }
