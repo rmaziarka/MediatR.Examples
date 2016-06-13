3108d30afbb82b9c479e4bd3237d3a7d38eef2dc
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-06-13 14:31:41 +0200

    Moved new side panel components to base folder - changed templates paths Reviewed by: Michał Jankowski

--- src/wwwroot/app/common/components/base/kfSidePanelCard/kfSidePanelCardComponent.ts
+++ src/wwwroot/app/common/components/base/kfSidePanelCard/kfSidePanelCardComponent.ts
@@ -2,7 +2,7 @@ module Antares.Common.Component {
     angular.module('app').component('kfSidePanelCard', {
         controller: 'kfSidePanelCardController',
         controllerAs: 'vm',
-        templateUrl: 'app/common/components/kfSidePanelCard/kfSidePanelCard.html',
+        templateUrl: 'app/common/components/base/kfSidePanelCard/kfSidePanelCard.html',
         transclude: {
             'content': '?sidePanelContent',
             'header': '?sidePanelHeader',
