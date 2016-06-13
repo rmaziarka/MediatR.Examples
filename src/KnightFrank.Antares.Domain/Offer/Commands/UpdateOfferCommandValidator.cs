2f860466c40c940cc95826e80b5e2620f394acb4
Piotr Izak <pizak@objectivity.co.uk>
2016-06-13 13:46:46 +0200

    #25185 #26725 Extended offer update command validation and refactored offer update command handler. Overrided two validation methods: entity exists and enum exists. Reviewed by: Łukasz Misztal

--- src/KnightFrank.Antares.Domain/Offer/Commands/UpdateOfferCommandValidator.cs
+++ src/KnightFrank.Antares.Domain/Offer/Commands/UpdateOfferCommandValidator.cs
@@ -13,6 +13,10 @@
             this.RuleFor(x => x.Price).GreaterThan(0);
             this.RuleFor(x => x.SpecialConditions).Length(0, 4000);
             this.RuleFor(x => x.OfferDate).NotEmpty();
+            this.RuleFor(x => x.MortgageLoanToValue)
+                .GreaterThanOrEqualTo(0)
+                .LessThanOrEqualTo(200);
+            this.RuleFor(x => x.ProgressComment).Length(0, 4000);
         }
     }
 }
