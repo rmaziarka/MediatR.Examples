e621f3e039e20d0fdede7346fc9336e0c0db3354
Aneela Ahmed <aneela.ahmed@knightfrank.com>
2016-06-13 11:37:49 +0100

    #22726 #26784 Unit tests Reviewed By: Andrew Wakeling

---
+++ src/Tests/KnightFrank.Antares.Domain.UnitTests/User/CommandHandlers/UpdateUserCommandHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿namespace KnightFrank.Antares.Domain.UnitTests.User.CommandHandlers
+{
+    using System;
+
+    using KnightFrank.Antares.Dal.Model.User;
+    using KnightFrank.Antares.Dal.Repository;
+    using KnightFrank.Antares.Domain.Common.BusinessValidators;
+    using KnightFrank.Antares.Domain.Common.Enums;
+    using KnightFrank.Antares.Domain.User.CommandHandlers;
+    using KnightFrank.Antares.Domain.User.Commands;
+
+    using Moq;
+
+    using Ploeh.AutoFixture.Xunit2;
+
+    using Xunit;
+
+    [Collection("UpdateUserCommandHandler")]
+    [Trait("FeatureTitle", "User")]
+    public class UpdateUserCommandHandlerTests : IClassFixture<BaseTestClassFixture>
+    {
+        [Theory]
+        [AutoMoqData]
+        public void Given_UpdateUserCommand_When_Handle_Then_ShouldUpdateUser(
+            [Frozen] Mock<IEntityValidator> entityValidator,
+            [Frozen] Mock<IEnumTypeItemValidator> enumTypeValidator,
+            [Frozen] Mock<IGenericRepository<User>> userRepository,
+            UpdateUserCommand command,
+            UpdateUserCommandHandler handler,
+            User user)
+        {
+            userRepository.Setup(r => r.GetById(command.Id)).Returns(user);
+
+            // Act
+            Guid userId = handler.Handle(command);
+
+            // Assert
+            Assert.Equal(command.Id, userId);
+            entityValidator.Verify(x => x.EntityExists(user, command.Id), Times.Once);
+            enumTypeValidator.Verify(x => x.ItemExists(EnumType.SalutationFormat, (Guid)command.SalutationFormatId), Times.Once);
+            userRepository.Verify(r => r.Save(), Times.Once());
+        }
+    }
+}
---
+++ src/Tests/KnightFrank.Antares.Domain.UnitTests/User/Commands/UpdateUserCommandValidatorTests.cs
@@ -0,0 +1,46 @@
+﻿namespace KnightFrank.Antares.Domain.UnitTests.User.Commands
+{
+    using System;
+
+    using FluentAssertions;
+    using FluentValidation.Resources;
+    using FluentValidation.Results;
+    using Xunit;
+
+    using KnightFrank.Antares.Domain.UnitTests.FixtureExtension;
+    using KnightFrank.Antares.Domain.User.Commands;
+
+   
+
+    [Collection("UpdateUserCommand")]
+    [Trait("FeatureTitle", "User")]
+    public class UpdateUserCommandValidatorTests : IClassFixture<BaseTestClassFixture>
+    {
+      [Theory]
+        [AutoMoqData]
+        public void Given_ValidUpdateOfferCommand_When_Validating_Then_IsValid(UpdateUserCommandValidator validator
+            , UpdateUserCommand command)
+        {
+            // Act
+            ValidationResult validationResult = validator.Validate(command);
+
+            // Assert
+            validationResult.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void Given_EmptyId_When_Validating_Then_IsInvalid(UpdateUserCommandValidator validator
+             , UpdateUserCommand command)
+        {
+            // Arrange
+            command.Id = Guid.Empty;
+
+            // Act
+            ValidationResult validationResult = validator.Validate(command);
+
+            // Assert
+            validationResult.IsInvalid(nameof(command.Id), nameof(Messages.notempty_error));
+        }     
+    }
+}
