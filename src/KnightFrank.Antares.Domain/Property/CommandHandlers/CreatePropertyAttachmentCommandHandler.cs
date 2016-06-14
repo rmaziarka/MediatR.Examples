80b1c5ac25163e8f7dafcc48dd0e0182400cce00
Michal Jankowski <mjankowski@objectivity.co.uk>
2016-06-14 12:42:38 +0200

    #22689 #25144 Add WebApi to upload file to Property Reviewed by Karolnina Czerniawska

---
+++ src/KnightFrank.Antares.Domain/Property/CommandHandlers/CreatePropertyAttachmentCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿namespace KnightFrank.Antares.Domain.Property.CommandHandlers
+{
+    using System;
+
+    using KnightFrank.Antares.Dal.Model.Attachment;
+    using KnightFrank.Antares.Dal.Model.Property;
+    using KnightFrank.Antares.Dal.Model.User;
+    using KnightFrank.Antares.Dal.Repository;
+    using KnightFrank.Antares.Domain.Attachment.Commands;
+    using KnightFrank.Antares.Domain.Common.BusinessValidators;
+
+    using MediatR;
+
+    using EnumType = KnightFrank.Antares.Domain.Common.Enums.EnumType;
+
+    public class CreatePropertyAttachmentCommandHandler : IRequestHandler<CreateEntityAttachmentCommand, Guid>
+    {
+        private readonly IEntityValidator entityValidator;
+        private readonly IEnumTypeItemValidator enumTypeItemValidator;
+        private readonly IGenericRepository<Property> propertyRepository;
+
+        public CreatePropertyAttachmentCommandHandler(
+            IEntityValidator entityValidator, 
+            IEnumTypeItemValidator enumTypeItemValidator,
+            IGenericRepository<Property> propertyRepository)
+        {
+            this.entityValidator = entityValidator;
+            this.enumTypeItemValidator = enumTypeItemValidator;
+            this.propertyRepository = propertyRepository;
+        }
+
+        public Guid Handle(CreateEntityAttachmentCommand message)
+        {
+            this.entityValidator.EntityExists<User>(message.Attachment.UserId);
+            this.enumTypeItemValidator.ItemExists(EnumType.PropertyDocumentType, message.Attachment.DocumentTypeId);
+
+            Property property = this.propertyRepository.GetById(message.EntityId);
+            this.entityValidator.EntityExists(property, message.EntityId);
+
+            var attachment = AutoMapper.Mapper.Map<Attachment>(message.Attachment);
+
+            property.Attachments.Add(attachment);
+            this.propertyRepository.Save();
+
+            return attachment.Id;
+        }
+    }
+}
