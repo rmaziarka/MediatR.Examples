733b2c7a99b89fd9c489235bb2b07d399a110271
Michal Jankowski <mjankowski@objectivity.co.uk>
2016-06-15 07:39:06 +0200

    #22689 #25144 Change to abstract CreateEntityAttachmentCommand Reviewed by Karolina Czerniawska

--- src/KnightFrank.Antares.Domain/Activity/CommandHandlers/CreateActivityAttachmentCommandHandler.cs
+++ src/KnightFrank.Antares.Domain/Activity/CommandHandlers/CreateActivityAttachmentCommandHandler.cs
@@ -1,48 +1,25 @@
 ﻿namespace KnightFrank.Antares.Domain.Activity.CommandHandlers
 {
-    using System;
-
     using KnightFrank.Antares.Dal.Model.Attachment;
     using KnightFrank.Antares.Dal.Model.Property.Activities;
-    using KnightFrank.Antares.Dal.Model.User;
     using KnightFrank.Antares.Dal.Repository;
     using KnightFrank.Antares.Domain.Activity.Commands;
+    using KnightFrank.Antares.Domain.Attachment.Commands;
     using KnightFrank.Antares.Domain.Common.BusinessValidators;
-
-    using MediatR;
 
     using EnumType = KnightFrank.Antares.Domain.Common.Enums.EnumType;
 
-    public class CreateActivityAttachmentCommandHandler : IRequestHandler<CreateActivityAttachmentCommand, Guid>
+    public class CreateActivityAttachmentCommandHandler :
+        CreateEntityAttachmentCommandHandler<Activity, CreateActivityAttachmentCommand>
     {
-        private readonly IEntityValidator entityValidator;
-        private readonly IEnumTypeItemValidator enumTypeItemValidator;
-        private readonly IGenericRepository<Activity> activityRepository;
-
-        public CreateActivityAttachmentCommandHandler(
-            IEntityValidator entityValidator, 
-            IEnumTypeItemValidator enumTypeItemValidator,
-            IGenericRepository<Activity> activityRepository)
+         public CreateActivityAttachmentCommandHandler(IEntityValidator entityValidator, IEnumTypeItemValidator enumTypeItemValidator, IGenericRepository<Activity> activityRepository)
+            : base(entityValidator, enumTypeItemValidator, activityRepository, EnumType.ActivityDocumentType)
         {
-            this.entityValidator = entityValidator;
-            this.enumTypeItemValidator = enumTypeItemValidator;
-            this.activityRepository = activityRepository;
         }
 
-        public Guid Handle(CreateActivityAttachmentCommand message)
+        protected override void AddAttachmentToEntity(Activity entity, Attachment attachment)
         {
-            this.entityValidator.EntityExists<User>(message.Attachment.UserId);
-            this.enumTypeItemValidator.ItemExists(EnumType.ActivityDocumentType, message.Attachment.DocumentTypeId);
-
-            Activity activity = this.activityRepository.GetById(message.EntityId);
-            this.entityValidator.EntityExists(activity, message.EntityId);
-
-            var attachment = AutoMapper.Mapper.Map<Attachment>(message.Attachment);
-
-            activity.Attachments.Add(attachment);
-            this.activityRepository.Save();
-
-            return attachment.Id;
+            entity.Attachments.Add(attachment);
         }
     }
 }
--- src/KnightFrank.Antares.Domain/Property/CommandHandlers/CreatePropertyAttachmentCommandHandler.cs
+++ src/KnightFrank.Antares.Domain/Property/CommandHandlers/CreatePropertyAttachmentCommandHandler.cs
@@ -1,48 +1,25 @@
 ﻿namespace KnightFrank.Antares.Domain.Property.CommandHandlers
 {
-    using System;
-
     using KnightFrank.Antares.Dal.Model.Attachment;
     using KnightFrank.Antares.Dal.Model.Property;
-    using KnightFrank.Antares.Dal.Model.User;
     using KnightFrank.Antares.Dal.Repository;
+    using KnightFrank.Antares.Domain.Attachment.Commands;
     using KnightFrank.Antares.Domain.Common.BusinessValidators;
     using KnightFrank.Antares.Domain.Property.Commands;
 
-    using MediatR;
-
     using EnumType = KnightFrank.Antares.Domain.Common.Enums.EnumType;
 
-    public class CreatePropertyAttachmentCommandHandler : IRequestHandler<CreatePropertyAttachmentCommand, Guid>
+    public class CreatePropertyAttachmentCommandHandler :
+        CreateEntityAttachmentCommandHandler<Property, CreatePropertyAttachmentCommand>
     {
-        private readonly IEntityValidator entityValidator;
-        private readonly IEnumTypeItemValidator enumTypeItemValidator;
-        private readonly IGenericRepository<Property> propertyRepository;
-
-        public CreatePropertyAttachmentCommandHandler(
-            IEntityValidator entityValidator, 
-            IEnumTypeItemValidator enumTypeItemValidator,
-            IGenericRepository<Property> propertyRepository)
+        public CreatePropertyAttachmentCommandHandler(IEntityValidator entityValidator, IEnumTypeItemValidator enumTypeItemValidator, IGenericRepository<Property> propertyRepository)
+            : base(entityValidator, enumTypeItemValidator, propertyRepository, EnumType.PropertyDocumentType)
         {
-            this.entityValidator = entityValidator;
-            this.enumTypeItemValidator = enumTypeItemValidator;
-            this.propertyRepository = propertyRepository;
         }
 
-        public Guid Handle(CreatePropertyAttachmentCommand message)
+        protected override void AddAttachmentToEntity(Property entity, Attachment attachment)
         {
-            this.entityValidator.EntityExists<User>(message.Attachment.UserId);
-            this.enumTypeItemValidator.ItemExists(EnumType.PropertyDocumentType, message.Attachment.DocumentTypeId);
-
-            Property property = this.propertyRepository.GetById(message.EntityId);
-            this.entityValidator.EntityExists(property, message.EntityId);
-
-            var attachment = AutoMapper.Mapper.Map<Attachment>(message.Attachment);
-
-            property.Attachments.Add(attachment);
-            this.propertyRepository.Save();
-
-            return attachment.Id;
+            entity.Attachments.Add(attachment);
         }
     }
 }
