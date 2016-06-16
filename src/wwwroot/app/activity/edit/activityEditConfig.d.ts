6dd9fe280c224742c356285a85119a78d2527141
Marta Dycjan <martakanak@gmail.com>
2016-06-16 09:21:32 +0200

    #24784 Edit activity - status
    Reviewed by: No Review

---
+++ src/wwwroot/app/activity/edit/activityEditConfig.d.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+declare module Antares.Activity {
+    import Attributes = Antares.Attributes;
+
+    interface IActivityEditConfig extends IActivityConfig {
+        activityStatus: Attributes.IActivityStatusEditControlConfig;
+    }
+}
