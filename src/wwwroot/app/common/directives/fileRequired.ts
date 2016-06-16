84d79a61f7fd6fd4a6f0cab08bbe48221d0d787a
Karolina Czerniawska <kczerniawska@objectivity.co.uk>
2016-06-16 13:09:49 +0200

    #22689 #27085 Fix bug with file upload - restore solution with external plugin and small fix for it Review by: Zbigniew Rubiński

--- src/wwwroot/app/common/directives/fileRequired.ts
+++ src/wwwroot/app/common/directives/fileRequired.ts
@@ -4,19 +4,32 @@ module Antares.Common.Directive {
     export class FileRequiredDirective implements ng.IDirective {
         restrict = "A";
         require = 'ngModel';
-
+        scope = {
+            ngModel: '=ngModel'
+        };
         link(scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController) {
-            var validateFileRequired =(inputValue: any) => {
-                var isValid = !!inputValue;
+
+            var validateFileRequired = (inputValue: any) => {
+                var isFileCleared = JSON.parse(String(attrs['isFileCleared']));
+                var isValid = !isFileCleared || !!inputValue;
 
                 ngModel.$setValidity('fileRequired', isValid);
-
                 return isValid;
             }
 
-            ngModel.$validators['fileRequired'] = (modelValue) => {
+            ngModel.$validators['fileRequired'] = (modelValue, scope) => {
                 return validateFileRequired(modelValue);
             };
+
+            attrs.$observe('isFileCleared', function () {
+                validateFileRequired(ngModel.$viewValue);
+            });
+
+            scope.$watch('ngModel', (newValue, oldValue) => {
+                if ((newValue !== undefined || oldValue !== undefined) && newValue !== oldValue) {
+                    ngModel.$setDirty();
+                }
+            });
         };
 
         static factory() {
