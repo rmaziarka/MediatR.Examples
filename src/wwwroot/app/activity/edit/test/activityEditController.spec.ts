9fc2bd86b8086a1f065f5fec38bc7a017dfe5428
Marta Dycjan <martakanak@gmail.com>
2016-06-20 09:18:16 +0200

    #24784 UT tests for edit activity controller (moved from deparments)
    Reviewed by: No Review

---
+++ src/wwwroot/app/activity/edit/test/activityEditController.spec.ts
@@ -0,0 +1,71 @@
+﻿/// <reference path="../../../typings/_all.d.ts" />
+module Antares {
+    import Business = Common.Models.Business;
+    import Dto = Common.Models.Dto;
+    import ActivityEditController = Activity.ActivityEditController;
+    import Enums = Common.Models.Enums;
+    import runDescribe = TestHelpers.runDescribe;
+
+    describe('Given edit activity controller', () => {
+        var scope: ng.IScope,
+            compile: ng.ICompileService,
+            element: ng.IAugmentedJQuery,
+            $http: ng.IHttpBackendService,
+            state: ng.ui.IStateService,
+            assertValidator: TestHelpers.AssertValidators,
+            controller: ActivityEditController;
+
+        var leadNegotiatorMock = TestHelpers.ActivityUserGenerator.generate(Enums.NegotiatorTypeEnum.LeadNegotiator);
+        var secondaryNegotiatorsMock = TestHelpers.ActivityUserGenerator.generateMany(3, Enums.NegotiatorTypeEnum.SecondaryNegotiator);
+
+        var activityStatuses = [
+            { id: "111", code: "PreAppraisal" },
+            { id: "testStatus222", code: "MarketAppraisal" },
+            { id: "333", code: "NotSelling" }
+        ];
+
+
+        beforeEach(inject((
+            $rootScope: ng.IRootScopeService,
+            $controller: ng.IControllerService,
+            $httpBackend: ng.IHttpBackendService) => {
+
+            // init
+            scope = $rootScope.$new();
+            $http = $httpBackend;
+
+            controller = <ActivityEditController>$controller('ActivityEditController');
+            controller.activity = new Business.Activity();
+            controller.activity.leadNegotiator = leadNegotiatorMock;
+            controller.activity.secondaryNegotiator = secondaryNegotiatorsMock;
+        }));
+
+        describe('when departmentIsRelatedWithNegotiator is called', () => {
+            type TestCaseForDepartmentsCheck = [Business.ActivityDepartment, number, Business.ActivityDepartment, string, boolean]; //[leadNegotiatorDepartment, secondaryNegotiatorIndex, secondaryNegotiatorDepartment, message]
+            var departmentToCheck = TestHelpers.ActivityDepartmentGenerator.generate();
+
+            runDescribe('and when depertment is related to ')
+                .data<TestCaseForDepartmentsCheck>([
+                    [TestHelpers.ActivityDepartmentGenerator.generate(), 0, TestHelpers.ActivityDepartmentGenerator.generate(), 'no negotiator', false],
+                    [departmentToCheck, 0, TestHelpers.ActivityDepartmentGenerator.generate(), 'lead negotiator', true],
+                    [TestHelpers.ActivityDepartmentGenerator.generate(), 0, departmentToCheck, 'secondary negotiator 0', true],
+                    [TestHelpers.ActivityDepartmentGenerator.generate(), 1, departmentToCheck, 'secondary negotiator 1', true]])
+                .dataIt((data: TestCaseForDepartmentsCheck) =>
+                    `${data[3]} then it should return ${data[4]}`)
+                .run((data: TestCaseForDepartmentsCheck) => {
+                    // arrange
+                    controller.activity.activityDepartments.push(departmentToCheck);
+
+                    controller.activity.leadNegotiator.user.departmentId = data[0].departmentId;
+                    controller.activity.leadNegotiator.user.department = data[0].department;
+
+                    controller.activity.secondaryNegotiator[data[1]].user.departmentId = data[2].departmentId;
+                    controller.activity.secondaryNegotiator[data[1]].user.department = data[2].department;
+
+                    // act / assert
+                    expect(controller.departmentIsRelatedWithNegotiator(departmentToCheck.department)).toBe(data[4]);
+                });
+        });
+
+    });
+}
