6ffe5996c4882aea71d518c81d236fe6595def53
Adam Lepkowski <alepkowski@objectivity.co.uk>
2016-06-21 12:38:49 +0200

    Reverted accidently removed specflow step for companies Reviewed by: Marta Dycjan

--- src/Tests/KnightFrank.Antares.UITests/Steps/CreateCompanySteps.cs
+++ src/Tests/KnightFrank.Antares.UITests/Steps/CreateCompanySteps.cs
@@ -47,61 +47,67 @@
             this.page = new CreateCompanyPage(this.driverContext).OpenCreateCompanyPage();
         }
 
+        [Then(@"Company form on create company page should be diaplyed")]
+        public void CheckIfCreateContactIsDisplayed()
+        {
+            Assert.True(this.page.IsAddCompanyFormPresent());
+        }
+
         [When(@"User fills in company details on create company page")]
         public void FillInCompanyData(Table table)
         {
             var details = table.CreateInstance<Company>();
 
-			this.scenarioContext.Set(details.WebsiteUrl, "Url");
+            this.scenarioContext.Set(details.WebsiteUrl, "Url");
 
-			this.page.SetCompanyName(details.Name);
+            this.page.SetCompanyName(details.Name);
             this.page.SetWebsite(details.WebsiteUrl);
             this.page.SetClientCareUrl(details.ClientCarePageUrl);
             this.page.SetClientCareStatus();
         }
 
-		[When(@"User selects contacts on create company page")]
-		public void SelectContactsForCompany(Table table)
-		{
-			this.page.AddContactToCompany().WaitForSidePanelToShow();
+        [When(@"User selects contacts on create company page")]
+        public void SelectContactsForCompany(Table table)
+        {
+            this.page.AddContactToCompany().WaitForSidePanelToShow();
 
-			IEnumerable<Contact> contacts = table.CreateSet<Contact>();
+            IEnumerable<Contact> contacts = table.CreateSet<Contact>();
 
-			foreach (Contact contact in contacts)
-			{
-				this.page.ContactsList.WaitForContactsListToLoad().SelectContact(contact.FirstName, contact.Surname);
-			}
-			this.page.ContactsList.SaveContact();
-			this.page.WaitForSidePanelToHide();
-		}
+            foreach (Contact contact in contacts)
+            {
+                this.page.ContactsList.WaitForContactsListToLoad().SelectContact(contact.FirstName, contact.Surname);
+            }
+            this.page.ContactsList.SaveContact();
+            this.page.WaitForSidePanelToHide();
+        }
 
-		[Then(@"List of company contacts should contain following contacts")]
-		public void CheckContactsList(Table table)
-		{
-			List<string> contacts =
-				table.CreateSet<Contact>().Select(contact => contact.FirstName + " " + contact.Surname).ToList();
+        [Then(@"List of company contacts should contain following contacts")]
+        public void CheckContactsList(Table table)
+        {
+            List<string> contacts =
+                table.CreateSet<Contact>().Select(contact => contact.FirstName + " " + contact.Surname).ToList();
 
-			List<string> selectedContacts = this.page.Contacts;
+            List<string> selectedContacts = this.page.Contacts;
 
-			Assert.Equal(contacts.Count, selectedContacts.Count);
-			contacts.ShouldBeEquivalentTo(selectedContacts);
-		}
+            Assert.Equal(contacts.Count, selectedContacts.Count);
+            contacts.ShouldBeEquivalentTo(selectedContacts);
+        }
 
-		[When(@"User clicks on website url icon")]
-		public void WhenUserClicksOnWebsiteUrlIcon()
-		{
-			this.page.ClickOnWebsiteLink();
-		}
+        [When(@"User clicks on website url icon")]
+        public void WhenUserClicksOnWebsiteUrlIcon()
+        {
+            this.page.ClickOnWebsiteLink();
+        }
 
-		[Then(@"url opens in new tab")]
-		public void ThenUrlOpensInNewTab()
-		{
-			var scenarioUrl = this.scenarioContext.Get<string>("Url");
+        [Then(@"url opens in new tab")]
+        public void ThenUrlOpensInNewTab()
+        {
+            var scenarioUrl = this.scenarioContext.Get<string>("Url");
 
-			Assert.True(this.page.CheckNewTab(scenarioUrl));
-		}
+            Assert.True(this.page.CheckNewTab(scenarioUrl));
+        }
 
-		[When(@"User clicks save company button on create company page")]
+        [When(@"User clicks save company button on create company page")]
         public void SaveCompany()
         {
             this.page.SaveCompany();
