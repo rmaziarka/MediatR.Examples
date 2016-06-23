7968f42e41712fa19957b349ed72c071a4b6e9f3
Piotr Izak <pizak@objectivity.co.uk>
2016-06-23 16:54:10 +0100

    #20243 #26757 - Added new Web API method to retrieve contact titles ; Reviewed by: Robert Smyth.

---
+++ src/KnightFrank.Antares.Domain/Contact/QueryHandlers/ContactTitleQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace KnightFrank.Antares.Domain.Contact.QueryHandlers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using KnightFrank.Antares.Dal.Model.Contacts;
+    using KnightFrank.Antares.Dal.Repository;
+    using KnightFrank.Antares.Domain.Contact.Queries;
+
+    using MediatR;
+
+    public class ContactTitleQueryHandler : IRequestHandler<ContactTitleQuery, IEnumerable<ContactTitle>>
+    {
+        private readonly IReadGenericRepository<ContactTitle> contactTitletRepository;
+
+        public ContactTitleQueryHandler(IReadGenericRepository<ContactTitle> contactTitletRepository)
+        {
+            this.contactTitletRepository = contactTitletRepository;
+        }
+
+        public IEnumerable<ContactTitle> Handle(ContactTitleQuery message)
+        {
+            return this.contactTitletRepository.GetWithInclude(x => x.Locale).ToList();
+        }
+    }
+}
