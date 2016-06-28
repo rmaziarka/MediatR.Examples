dbac74a2d9da56572993bf7d9663c954b267dd2b
Kamil Haładus <khaladus@objectibity.co.uk>
2016-06-28 11:10:43 +0200

    #27025 #28113 Handling triple state of side panel component. Reviewed by : Radosław Maziarka

--- src/wwwroot/app/common/components/base/kfSidePanel/baseSidePanelController.ts
+++ src/wwwroot/app/common/components/base/kfSidePanel/baseSidePanelController.ts
@@ -1,35 +1,25 @@
 /// <reference path="../../../../typings/_all.d.ts" />
 
 module Antares.Common.Component {
+    import Enums = Antares.Common.Models.Enums;
     export abstract class BaseSidePanelController {
-
-        isVisible: boolean;
-        stateChanged: boolean = false;
+                
         isBusy: boolean;
+        isVisible: Enums.SidePanelState = Enums.SidePanelState.Untouched;
 
         protected panelShown = () => {
-                };
+        };
 
-                protected panelHidden = () => {
-                };
-
-        show = () => {
-            this.stateChanged = true;
-            this.panelShown();
-        }
-
-                hide = () => {
-                    this.stateChanged = true;
-                    this.panelHidden();
-                }
+        protected panelHidden = () => {
+        };        
 
         public $onChanges(changesObj: IBaseSidePanelChange) {
             if (changesObj.isVisible && changesObj.isVisible.currentValue !== changesObj.isVisible.previousValue) {
-                if (changesObj.isVisible.currentValue === true) {
-                    this.show();
+                if (changesObj.isVisible.currentValue === Enums.SidePanelState.Opened) {
+                    this.panelShown();
                 }
-                else if (changesObj.isVisible.currentValue === false && !changesObj.isVisible.isFirstChange()) {
-                    this.hide();
+                else if (changesObj.isVisible.currentValue === Enums.SidePanelState.Closed) {
+                    this.panelShown();
                 }
             }
         }
