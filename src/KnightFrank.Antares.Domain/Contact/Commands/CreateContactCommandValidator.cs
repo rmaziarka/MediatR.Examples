5e50f09169d77b7ee19977d95c30a44394a9303c
Andrew Wakeling <andrew.wakeling@knightfrank.com>
2016-06-29 10:54:18 +0100

    #19844 #28159 Bug fix Reviewed by: Aneela Ahmed

--- src/KnightFrank.Antares.Domain/Contact/Commands/CreateContactCommandValidator.cs
+++ src/KnightFrank.Antares.Domain/Contact/Commands/CreateContactCommandValidator.cs
@@ -7,23 +7,22 @@ namespace KnightFrank.Antares.Domain.Contact.Commands
         public CreateContactCommandValidator()
         {
 			this.RuleFor(x => x.Title).NotEmpty().Length(1, 128);
-			this.RuleFor(x => x.FirstName).Length(1, 128);
+			this.RuleFor(x => x.FirstName).Length(0, 128);
             this.RuleFor(x => x.LastName).NotEmpty().Length(1, 128);
 
 			this.RuleFor(x => x.MailingFormalSalutation).Length(1, 128);
 			this.RuleFor(x => x.MailingSemiformalSalutation).Length(1, 128);
-			this.RuleFor(x => x.MailingInformalSalutation).Length(1, 128);
-			this.RuleFor(x => x.MailingPersonalSalutation).Length(1, 128);
+			this.RuleFor(x => x.MailingInformalSalutation).Length(0, 128);
+			this.RuleFor(x => x.MailingPersonalSalutation).Length(0, 128);
 			this.RuleFor(x => x.MailingEnvelopeSalutation).Length(1, 128);
 
-			this.RuleFor(x => x.EventInviteSalutation).Length(1, 128);
-			this.RuleFor(x => x.EventSemiformalSalutation).Length(1, 128);
-			this.RuleFor(x => x.EventInformalSalutation).Length(1, 128);
-			this.RuleFor(x => x.EventPersonalSalutation).Length(1, 128);
-			this.RuleFor(x => x.EventEnvelopeSalutation).Length(1, 128);
+			this.RuleFor(x => x.EventInviteSalutation).Length(0, 128);
+			this.RuleFor(x => x.EventSemiformalSalutation).Length(0, 128);
+			this.RuleFor(x => x.EventInformalSalutation).Length(0, 128);
+			this.RuleFor(x => x.EventPersonalSalutation).Length(0, 128);
+			this.RuleFor(x => x.EventEnvelopeSalutation).Length(0, 128);
 
             this.RuleFor(x => x.LeadNegotiator).NotNull().SetValidator(new ContactUserValidator());
-
         }
 	}
 }
