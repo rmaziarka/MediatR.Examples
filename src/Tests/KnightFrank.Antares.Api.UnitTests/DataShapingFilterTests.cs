a431547d200d43c32509053ff11442643c2fa8aa
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-06-29 11:48:05 +0200

    #24785 fix data shaping filter tests Reviewed by: Zbigniew Rubinski

--- src/Tests/KnightFrank.Antares.Api.UnitTests/DataShapingFilterTests.cs
+++ src/Tests/KnightFrank.Antares.Api.UnitTests/DataShapingFilterTests.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KnightFrank.Antares.Api.UnitTests
+﻿namespace KnightFrank.Antares.Api.UnitTests
 {
+    using System.Collections.Generic;
     using System.Net.Http;
     using System.Net.Http.Formatting;
     using System.Web.Http.Controllers;
@@ -16,6 +11,7 @@ namespace KnightFrank.Antares.Api.UnitTests
     using KnightFrank.Antares.Dal.Model.Property.Activities;
     using KnightFrank.Antares.Domain.AttributeConfiguration.Common;
     using KnightFrank.Antares.Domain.AttributeConfiguration.Enums;
+    using KnightFrank.Antares.Domain.Common;
     using KnightFrank.Antares.Tests.Common.Extensions.AutoFixture.Attributes;
 
     using Moq;
@@ -29,28 +25,38 @@ namespace KnightFrank.Antares.Api.UnitTests
         [AutoMoqData]
         [Theory]
         public void Given_EmptyExecutedContext_When_OnActionExecuted_Then_NullifyDisallowedValuesNotInvoked(
+            [Frozen] Mock<INinjectInstanceResolver> ninjectInstanceResolver,
             [Frozen] Mock<IEntityMapper<Activity>> activityEntityMapper,
+            [Frozen] Mock<IEntityMapper<Requirement>> requirementEntityMapper,
+            [Frozen] Mock<IEntityMapper<Offer>> offerEntityMapper,
             DataShapingFilter dataShapingFilter)
         {
             // Arrange
             var emptyContext = new HttpActionExecutedContext();
+            ninjectInstanceResolver.Setup(x => x.GetInstance<IEntityMapper<Activity>>()).Returns(activityEntityMapper.Object);
+            ninjectInstanceResolver.Setup(x => x.GetInstance<IEntityMapper<Requirement>>()).Returns(requirementEntityMapper.Object);
+            ninjectInstanceResolver.Setup(x => x.GetInstance<IEntityMapper<Offer>>()).Returns(offerEntityMapper.Object);
 
             // Act
             dataShapingFilter.OnActionExecuted(emptyContext);
 
             // Assert
             activityEntityMapper.Verify(c => c.NullifyDisallowedValues(It.IsAny<Activity>(), It.IsAny<PageType>()), Times.Never);
+            requirementEntityMapper.Verify(c => c.NullifyDisallowedValues(It.IsAny<Requirement>(), It.IsAny<PageType>()), Times.Never);
+            offerEntityMapper.Verify(c => c.NullifyDisallowedValues(It.IsAny<Offer>(), It.IsAny<PageType>()), Times.Never);
         }
 
         [AutoMoqData]
         [Theory]
         public void Given_ExecutedContextWithActivity_When_OnActionExecuted_Then_NullifyDisallowedValuesInvokedOnce(
+            [Frozen] Mock<INinjectInstanceResolver> ninjectInstanceResolver, 
             [Frozen] Mock<IEntityMapper<Activity>> activityEntityMapper,
             DataShapingFilter dataShapingFilter,
             Activity activity)
         {
             // Arrange
             HttpActionExecutedContext activityContext = this.CreateContext(activity);
+            ninjectInstanceResolver.Setup(x => x.GetInstance<IEntityMapper<Activity>>()).Returns(activityEntityMapper.Object);
 
             // Act
             dataShapingFilter.OnActionExecuted(activityContext);
@@ -62,6 +68,7 @@ namespace KnightFrank.Antares.Api.UnitTests
         [AutoMoqData]
         [Theory]
         public void Given_ExecutedContextWithOffer_When_OnActionExecuted_Then_NullifyDisallowedValuesInvokedOnce(
+            [Frozen] Mock<INinjectInstanceResolver> ninjectInstanceResolver, 
             [Frozen] Mock<IEntityMapper<Activity>> activityEntityMapper,
             DataShapingFilter dataShapingFilter,
             Offer offer,
@@ -70,6 +77,7 @@ namespace KnightFrank.Antares.Api.UnitTests
             // Arrange
             offer.Activity = activity;
             HttpActionExecutedContext activityContext = this.CreateContext(offer);
+            ninjectInstanceResolver.Setup(x => x.GetInstance<IEntityMapper<Activity>>()).Returns(activityEntityMapper.Object);
 
             // Act
             dataShapingFilter.OnActionExecuted(activityContext);
@@ -81,6 +89,7 @@ namespace KnightFrank.Antares.Api.UnitTests
         [AutoMoqData]
         [Theory]
         public void Given_ExecutedContextWithPropertyWithActivities_When_OnActionExecuted_Then_NullifyDisallowedValuesInvokedForEachActivity(
+            [Frozen] Mock<INinjectInstanceResolver> ninjectInstanceResolver,
             [Frozen] Mock<IEntityMapper<Activity>> activityEntityMapper,
             DataShapingFilter dataShapingFilter,
             Property property,
@@ -90,6 +99,7 @@ namespace KnightFrank.Antares.Api.UnitTests
             property.Activities = activities;
 
             HttpActionExecutedContext activityContext = this.CreateContext(property);
+            ninjectInstanceResolver.Setup(x => x.GetInstance<IEntityMapper<Activity>>()).Returns(activityEntityMapper.Object);
 
             // Act
             dataShapingFilter.OnActionExecuted(activityContext);
