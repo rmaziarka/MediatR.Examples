b072fb21d4918e7cb01d97a927b666b59284c3d2
Zbigniew Rubiński <zrubinski@objectivity.co.uk>
2016-06-30 11:23:48 +0200

    #25385 #27768 Add price control
    Reviewed by: Michał Lenartowicz

--- src/wwwroot/app/offer/previewCard/offerPreviewCardController.ts
+++ src/wwwroot/app/offer/previewCard/offerPreviewCardController.ts
@@ -9,6 +9,40 @@ module Antares.Offer {
         offer: Dto.IOffer;
         onEdit: () => void;
 
+        // controls
+        controlSchemas: any = {
+            status: <Attributes.IEnumItemControlSchema> {
+                controlId: "offer-status",
+                translationKey: "OFFER.PREVIEW.STATUS",
+                enumTypeCode: Dto.EnumTypeCode.OfferStatus
+            },
+            price: <Attributes.IPriceControlSchema> {
+                controlId: "offer-price",
+                translationKey: "OFFER.PREVIEW.OFFER"
+            },
+            pricePerWeek: <Attributes.IPriceControlSchema> {
+                controlId: "offer-price-per-week",
+                translationKey: "OFFER.PREVIEW.OFFER",
+                suffix: "OFFER.PREVIEW.OFFER_PER_WEEK"
+            },
+            offerDate: <Attributes.IDateControlSchema> {
+                controlId: "offer-date",
+                translationKey: "OFFER.PREVIEW.OFFER_DATE"
+            },
+            exchangeDate: <Attributes.IDateControlSchema> {
+                controlId: "offer-exchange-date",
+                translationKey: "OFFER.PREVIEW.EXCHANGE_DATE"
+            },
+            completionDate: <Attributes.IDateControlSchema> {
+                controlId: "offer-completion-date",
+                translationKey: "OFFER.PREVIEW.COMPLETION_DATE",
+            },
+            specialConditions: <Attributes.ITextControlSchema> {
+                controlId: "offer-special-conditions",
+                translationKey: "OFFER.PREVIEW.SPECIAL_CONDITIONS"
+            }
+        }
+
         edit = () => {
             this.onEdit();
         }
