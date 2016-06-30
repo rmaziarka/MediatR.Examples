a619cee8bca4c668bad158c055ed03bca54dc083
Piotr Izak <pizak@objectivity.co.uk>
2016-06-30 13:51:44 +0200

    Fixed company contact generator; Reviewed by: Andrzej Nowik.

--- src/wwwroot/app/common/testHelpers/companyGenerator.test.ts
+++ src/wwwroot/app/common/testHelpers/companyGenerator.test.ts
@@ -5,15 +5,16 @@ module Antares.TestHelpers {
     import Dto = Common.Models.Dto;
 
     export class CompanyGenerator {
-        public static generateDto(): Dto.ICompany{
+        public static generateDto(): Dto.ICompany {
 
             var company: Dto.ICompany = {
-                id : CompanyGenerator.makeRandom('id'),
-                name : CompanyGenerator.makeRandom('name'),
-                websiteUrl : CompanyGenerator.makeRandom('website'),
+                id: CompanyGenerator.makeRandom('id'),
+                name: CompanyGenerator.makeRandom('name'),
+                websiteUrl: CompanyGenerator.makeRandom('website'),
                 clientCarePageUrl: CompanyGenerator.makeRandom('clientCarePageUrl'),
                 clientCareStatusId: CompanyGenerator.makeRandom('clientCareStatusId'),
                 clientCareStatus: <Dto.IEnumTypeItem>{},
+                companiesContacts: [],
                 contacts: ContactGenerator.generateMany(3)
             }
 
@@ -25,7 +26,7 @@ module Antares.TestHelpers {
         }
 
         public static generateMany(n: number): Business.Company[] {
-            return _.map(CompanyGenerator.generateManyDtos(n), (company: Dto.ICompany) => { return new Business.Company(company); });
+            return _.map<Dto.ICompany, Business.Company>(CompanyGenerator.generateManyDtos(n), (company: Dto.ICompany) => new Business.Company(company));
         }
 
         public static generate(): Business.Company {
