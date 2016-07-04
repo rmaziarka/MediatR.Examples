b62e0360287c407691301385dc1059711da70e99
Ryszard Seniuta <rseniuta@objectivity.co.uk>
2016-07-04 12:05:13 +0200

    #27782 #25326 Add company relationships to offer table
    Reviewed by Pawel Zapalowski

--- src/KnightFrank.Antares.Domain/Offer/CommandHandlers/UpdateOfferCommandHandler.cs
+++ src/KnightFrank.Antares.Domain/Offer/CommandHandlers/UpdateOfferCommandHandler.cs
@@ -63,9 +63,9 @@
 
             this.entityValidator.EntityExists(offer, message.Id);
             this.entityValidator.EntityExists<Contact>(message.ApplicantSolicitorId);
-            this.entityValidator.EntityExists<Company>( message.ApplicantSolicitorCompanyId);
+            this.entityValidator.EntityExists<Company>(message.ApplicantSolicitorCompanyId);
             this.entityValidator.EntityExists<Contact>(message.VendorSolicitorId);
-            this.entityValidator.EntityExists<Company>( message.VendorSolicitorCompanyId);
+            this.entityValidator.EntityExists<Company>(message.VendorSolicitorCompanyId);
 
             // ReSharper disable once PossibleNullReferenceException
             var offerType = EnumExtensions.ParseEnum<OfferType>(offer.OfferType.EnumCode);
