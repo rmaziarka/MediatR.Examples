6e5ac7410530665c9108e32c51d6f96b88f9e047
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-07-07 13:56:10 +0200

    #27011 #29131 update requirement card template Reviewed by: Michal Lenartowicz

--- src/wwwroot/app/common/components/requirement/requirementCardController.ts
+++ src/wwwroot/app/common/components/requirement/requirementCardController.ts
@@ -2,9 +2,21 @@
 
 module Antares.Common.Component {
     import Business = Common.Models.Business;
+    import Dto = Common.Models.Dto;
 
     export class RequirementCardController {
         requirement: Business.Requirement;
+
+        constructor(
+            private $state: ng.ui.IStateService,
+            private appConfig: Common.Models.IAppConfig,
+            private $window: ng.IWindowService) {
+        }
+
+        navigateToRequirement = (requirement: Dto.IRequirement) => {
+            var requirementViewUrl = this.appConfig.appRootUrl + this.$state.href('app.requirement-view', { id: requirement.id }, { absolute: false });
+            this.$window.open(requirementViewUrl, '_blank');
+        }
     }
 
     angular.module('app').controller('requirementCardController', RequirementCardController);
