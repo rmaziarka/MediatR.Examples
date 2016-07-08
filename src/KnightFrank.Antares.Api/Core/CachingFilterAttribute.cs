1f2d9ed6c6cdb9fd8565406d8adc3d3b1dd5e55e
Andrew Wakeling <andrew.wakeling@knightfrank.com>
2016-07-08 16:23:37 +0100

    #28375 #29162 Caching test Reviewed by: No-one

---
+++ src/KnightFrank.Antares.Api/Core/CachingFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace KnightFrank.Antares.Api.Core
+{
+    using System.Web.Http.Filters;
+
+	/// <summary>
+	/// CachingFilterAttribute
+	/// </summary>
+	public class CachingFilterAttribute : ExceptionFilterAttribute
+    {
+		/// <summary>
+		/// OnResultExecuted
+		/// </summary>
+		/// <param name="context"></param>
+		public void OnResultExecuted(HttpActionExecutedContext context)
+		{
+			context.Response.Headers.Add("Cache-Control", "no-cache");
+		}
+	}
+}
