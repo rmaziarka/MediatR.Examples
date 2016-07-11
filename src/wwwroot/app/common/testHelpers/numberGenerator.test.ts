b54d3bacc6504559ff75132bfb24b2b097898759
Michal Jankowski <mjankowski@objectivity.co.uk>
2016-07-11 11:13:16 +0200

    #27101 #27900 Add number generator Reviewed by Marta Dycjan

---
+++ src/wwwroot/app/common/testHelpers/numberGenerator.test.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.TestHelpers {
+    export class NumberGenerator {
+        public static generate() {
+            return Math.round(Math.random()*1000) + 1;
+        }
+    }
+}
