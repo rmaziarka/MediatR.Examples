2410f76f9db250b76d29b43fb8d2c9dcbee537ce
Michal Lenartowicz <mlenartowicz@objectivity.co.uk>
2016-07-12 11:25:28 +0200

    #25182 Merge develop into chains - run custom tool on tt changes Reviewed by: Pawel Zapalowski

--- src/KnightFrank.Antares.Domain/Common/Enums/GenerateEnumTypeItems.cs
+++ src/KnightFrank.Antares.Domain/Common/Enums/GenerateEnumTypeItems.cs
@@ -3,71 +3,76 @@
 // ReSharper disable InconsistentNaming
 namespace KnightFrank.Antares.Domain.Common.Enums
 {
-    public enum EnumType
-    {
-        EntityType,
-        OwnershipType,
-        ActivityStatus,
-        Division,
-        ActivityDocumentType,
-        RequirementDocumentType,
-        PropertyDocumentType,
-        OfferStatus,
-        UserType,
-        ActivityDepartmentType,
-        MortgageStatus,
-        ClientCareStatus,
-        MortgageSurveyStatus,
-        AdditionalSurveyStatus,
-        SearchStatus,
-        Enquiries,
-        SalutationFormat,
-        MailingSalutation,
-        EventSalutation,
-        ActivitySource,
-        ActivitySellingReason,
-        DisposalType,
-        Decoration,
-    }
+	public enum EnumType
+	{
+	 	EntityType,
+	 	OwnershipType,
+	 	ActivityStatus,
+	 	Division,
+	 	ActivityDocumentType,
+	 	RequirementDocumentType,
+	 	PropertyDocumentType,
+	 	OfferStatus,
+	 	UserType,
+	 	ActivityDepartmentType,
+	 	MortgageStatus,
+	 	ChainMortgageStatus,
+	 	ClientCareStatus,
+	 	MortgageSurveyStatus,
+	 	ChainMortgageSurveyStatus,
+	 	AdditionalSurveyStatus,
+	 	SearchStatus,
+	 	ChainSearchStatus,
+	 	Enquiries,
+	 	ChainEnquiries,
+	 	ChainContractAgreedStatus,
+	 	SalutationFormat,
+	 	ActivitySource,
+	 	ActivitySellingReason,
+	 	DisposalType,
+	 	Decoration,
+	 	MailingSalutation,
+	 	EventSalutation,
+	 }
 
+	
+	public enum EntityType
+	{
+		Property,
+		Requirement,
+	}
 
-    public enum EntityType
-    {
-        Property,
-        Requirement,
-    }
+	public enum OwnershipType
+	{
+		Freeholder,
+		Leaseholder,
+	}
 
-    public enum OwnershipType
-    {
-        Freeholder,
-        Leaseholder,
-    }
+	public enum ActivityStatus
+	{
+		PreAppraisal,
+		MarketAppraisal,
+		NotSelling,
+	}
 
-    public enum ActivityStatus
-    {
-        PreAppraisal,
-        MarketAppraisal,
-        NotSelling,
-    }
+	public enum Division
+	{
+		Residential,
+		Commercial,
+	}
 
-    public enum Division
-    {
-        Residential,
-        Commercial,
-    }
-
-    public enum ActivityDocumentType
-    {
-        TermsOfBusiness,
-        MarketingSignOff,
-        CDDDocument,
-        Photograph,
-        FloorPlan,
-        Brochure,
-        VideoTour,
-        EPC,
-        GasCertificate,
-    }
+	public enum ActivityDocumentType
+	{
+		TermsOfBusiness,
+		MarketingSignOff,
+		CDDDocument,
+		Photograph,
+		FloorPlan,
+		Brochure,
+		VideoTour,
+		EPC,
+		GasCertificate,
+	}
 
 	public enum RequirementDocumentType
 	{
@@ -82,34 +87,42 @@ namespace KnightFrank.Antares.Domain.Common.Enums
 		VideoTour,
 	}
 
-    public enum OfferStatus
-    {
-        New,
-        Withdrawn,
-        Rejected,
-        Accepted,
-    }
+	public enum OfferStatus
+	{
+		New,
+		Withdrawn,
+		Rejected,
+		Accepted,
+	}
 
-    public enum UserType
-    {
-        LeadNegotiator,
-        SecondaryNegotiator,
-    }
+	public enum UserType
+	{
+		LeadNegotiator,
+		SecondaryNegotiator,
+	}
 
-    public enum ActivityDepartmentType
-    {
-        Managing,
-        Standard,
-    }
+	public enum ActivityDepartmentType
+	{
+		Managing,
+		Standard,
+	}
 
-    public enum MortgageStatus
-    {
-        Unknown,
-        NotRequiredCashFromSale,
-        NotRequiredCashInBank,
-        InProgress,
-        Agreed,
-    }
+	public enum MortgageStatus
+	{
+		Agreed,
+		Unknown,
+		NotRequiredCashFromSale,
+		NotRequiredCashInBank,
+		InProgress,
+	}
+
+	public enum ChainMortgageStatus
+	{
+		Unknown,
+		Complete,
+		Outstanding,
+		NotRequired,
+	}
 
 	public enum ClientCareStatus
 	{
@@ -118,56 +131,66 @@ namespace KnightFrank.Antares.Domain.Common.Enums
 		KeyClient,
 	}
 
-    public enum MortgageSurveyStatus
-    {
-        Unknown,
-        NotRequired,
-        Complete,
-        Outstanding,
-    }
+	public enum MortgageSurveyStatus
+	{
+		Unknown,
+		NotRequired,
+		Complete,
+		Outstanding,
+	}
 
-    public enum AdditionalSurveyStatus
-    {
-        Unknown,
-        NotRequired,
-        InProgress,
-        Complete,
-    }
+	public enum ChainMortgageSurveyStatus
+	{
+		Unknown,
+		Complete,
+		Outstanding,
+		NotRequired,
+	}
 
-    public enum SearchStatus
-    {
-        NotStarted,
-        AppliedFor,
-        Complete,
-    }
+	public enum AdditionalSurveyStatus
+	{
+		Unknown,
+		NotRequired,
+		InProgress,
+		Complete,
+	}
 
-    public enum Enquiries
-    {
-        NotStarted,
-        Sent,
-        Complete,
-    }
+	public enum SearchStatus
+	{
+		NotStarted,
+		AppliedFor,
+		Complete,
+	}
+
+	public enum ChainSearchStatus
+	{
+		Complete,
+		Outstanding,
+	}
+
+	public enum Enquiries
+	{
+		NotStarted,
+		Sent,
+		Complete,
+	}
+
+	public enum ChainEnquiries
+	{
+		Complete,
+		Outstanding,
+	}
+
+	public enum ChainContractAgreedStatus
+	{
+		Complete,
+		Outstanding,
+	}
 
 	public enum SalutationFormat
 	{
 		MrJohnSmith,
 		JohnSmithEsq,
-	}
-
-	public enum MailingSalutation
-	{
-		MailingFormal,
-		MailingSemiformal,
-		MailingInformal,
-		MailingPersonal,
-	}
-
-	public enum EventSalutation
-	{
-		EventInvite,
-		EventSemiformal,
-		EventInformal,
-		EventPersonal,
 	}
 
 	public enum ActivitySource
@@ -228,5 +251,21 @@ namespace KnightFrank.Antares.Domain.Common.Enums
 		Fair,
 		Good,
 		VeryGood,
+	}
+
+	public enum MailingSalutation
+	{
+		MailingFormal,
+		MailingSemiformal,
+		MailingInformal,
+		MailingPersonal,
+	}
+
+	public enum EventSalutation
+	{
+		EventInvite,
+		EventSemiformal,
+		EventInformal,
+		EventPersonal,
 	}
 }
