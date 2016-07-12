4e3f0f2f816c529e2efc4c50d408de97d71b9c23
Marta Dycjan <mdycjan@objectivity.co.uk>
2016-07-12 15:12:31 +0200

    #24787 Types for configuration
    Reviewed By: Kamil Haladus

---
+++ src/wwwroot/app/tenancy/edit/tenancyTermEditControlConfig.d.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+declare module Antares.Tenancy {
+    interface ITenancyTermEditControlConfig extends Antares.Common.Models.Dto.IControlConfig  {
+        term: Antares.Common.Models.Dto.IFieldConfig;
+    }
+}
