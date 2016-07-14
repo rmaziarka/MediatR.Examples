4b6e530269ca4e054a676f60d8818569d1291034
Marta Dycjan <mdycjan@objectivity.co.uk>
2016-07-14 09:59:24 +0200

    #24787 WebApi IT - happy path for create tenancy
    Reviewed By: Patryk Kulecki

---
+++ src/Tests/KnightFrank.Antares.Api.IntegrationTests/Steps/Tenancies/TenancySteps.cs
@@ -0,0 +1,81 @@
+﻿namespace KnightFrank.Antares.Api.IntegrationTests.Steps.Tenancies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+
+    using FluentAssertions;
+
+    using KnightFrank.Antares.Api.IntegrationTests.Extensions;
+    using KnightFrank.Antares.Api.IntegrationTests.Fixtures;
+    using KnightFrank.Antares.Dal.Model.Property;
+    using KnightFrank.Antares.Dal.Model.Property.Activities;
+    using KnightFrank.Antares.Dal.Model.Tenancy;
+    using KnightFrank.Antares.Domain.Tenancy.Commands;
+
+    using Newtonsoft.Json;
+
+    using TechTalk.SpecFlow;
+
+    using Xunit;
+
+    [Binding]
+    public class TenancySteps : IClassFixture<BaseTestClassFixture>
+    {
+        private const string ApiUrl = "/api/tenancies";
+        private readonly BaseTestClassFixture fixture;
+        private readonly ScenarioContext scenarioContext;
+
+        public TenancySteps(BaseTestClassFixture fixture, ScenarioContext scenarioContext)
+        {
+            this.fixture = fixture;
+            if (scenarioContext == null)
+            {
+                throw new ArgumentNullException(nameof(scenarioContext));
+            }
+            this.scenarioContext = scenarioContext;
+        }
+
+        [When(@"User creates tenancy for latest requirement and activity")]
+        public void WhenUserCreatesTenancyForLatestRequirementAndActivity()
+        {
+            Guid requirementId = this.scenarioContext.Get<Requirement>("Requirement").Id;
+            Guid activityId = this.scenarioContext.Get<Activity>("Activity").Id;
+
+            var tenancyCommand = new CreateTenancyCommand
+            {
+                ActivityId = activityId,
+                RequirementId = requirementId,
+                LandlordContacts = new List<Guid>(),
+                TenantContacts = new List<Guid>(),
+                Term = new UpdateTenancyTerm
+                {
+                    AgreedRent = 200,
+                    StartDate = new DateTime(2016, 2, 2),
+                    EndDate = new DateTime(2017, 2, 2)
+                }
+            };
+
+            this.PostTenancy(tenancyCommand);
+        }
+
+        [Then(@"Tenancy should be the same as added")]
+        public void ThenTenancyShouldBeTheSameAsAdded()
+        {
+            var currectTenancy = JsonConvert.DeserializeObject<Tenancy>(this.scenarioContext.GetResponseContent());
+            Tenancy expectedTenancy =
+                this.fixture.DataContext.Tenancies.Single(req => req.Id.Equals(currectTenancy.Id));
+
+            expectedTenancy.ActivityId.Should().Be(currectTenancy.ActivityId);
+            expectedTenancy.RequirementId.Should().Be(currectTenancy.RequirementId);
+            expectedTenancy.Terms.First().ShouldBeEquivalentTo(currectTenancy.Terms.First());
+        }
+
+        private void PostTenancy(CreateTenancyCommand command)
+        {
+            HttpResponseMessage response = this.fixture.SendPostRequest(ApiUrl, command);
+            this.scenarioContext.SetHttpResponseMessage(response);
+        }
+    }
+}
---
+++ src/Tests/KnightFrank.Antares.Api.IntegrationTests/Tests/Tenancies/Tenancy.feature
@@ -0,0 +1,17 @@
+﻿Feature: Tenancy 
+
+@Tenancies
+Scenario: Create tenancy
+	Given Property exists in database
+		| PropertyType | Division    |
+		| House        | Residential |
+		And Activity exists in database
+			| ActivityStatus | ActivityType  |
+			| PreAppraisal   | Freehold Sale |
+		And Contacts exists in database
+		| Title  | FirstName | LastName |
+		| Mister | Tomasz    | Bien     |
+		And Requirement of type ResidentialLetting exists in database
+	When User creates tenancy for latest requirement and activity
+	Then User should get OK http status code
+		And Tenancy should be the same as added
