ff3401b37ce7d06f3ad7cf656133887c6dfb5727
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-07-15 12:45:20 +0200

    #27134 #28887 checkbox list edit control; list view control Reviewed by: Bartosz Szatanik

---
+++ src/wwwroot/app/attributes/listView/listViewControlController.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="../../typings/_all.d.ts" />
+
+module Antares.Attribues {
+    export class ListViewControlController {
+        // bindings 
+        public ngModel: any[];
+        public config: any;
+        public schema: Attributes.IListViewControlSchema;
+        public itemTemplateUrl: string;
+
+        private defaultItemTemplateUrl: string = 'app/attributes/listView/templates/listItemSimpleTemplate.html';
+
+        constructor() {
+            this.ngModel = this.ngModel || [];
+            this.itemTemplateUrl = this.itemTemplateUrl || this.defaultItemTemplateUrl;
+        }
+    }
+
+    angular.module('app').controller('ListViewControlController', ListViewControlController);
+}
