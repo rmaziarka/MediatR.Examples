08517a26a3480e4f4e963e2ad7f773abf1f56292
Pawel Zapalowski <pzapalowski@objectivity.co.uk>
2016-07-18 19:10:26 +0200

    #27134 #28897 checkbox list edit control unit tests Reviewed by: Bartosz Szatanik

--- src/wwwroot/app/attributes/checkboxList/checkboxListEditControlSchema.d.ts
+++ src/wwwroot/app/attributes/checkboxList/checkboxListEditControlSchema.d.ts
@@ -4,7 +4,7 @@ declare module Antares.Attributes {
     interface ICheckboxListEditControlSchema extends IListViewControlSchema {
         formName: string,
         fieldName: string;
-        compareMember: string;
+        compareMember?: string;
         checkboxes: ICheckboxSchema[]
     }
 
