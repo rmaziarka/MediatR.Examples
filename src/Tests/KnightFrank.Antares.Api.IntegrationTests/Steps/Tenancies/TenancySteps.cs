d710f8559e19e1c5ae6d60ce74ef0230a6915cfb
Slawomir Pacia <spacia@objectivity.co.uk>
2016-07-19 12:58:04 +0200

    [#24787][#28854] add tenancy integration tests Reviewed by: Tomasz Bien

--- src/Tests/KnightFrank.Antares.Api.IntegrationTests/Steps/Tenancies/TenancySteps.cs
+++ src/Tests/KnightFrank.Antares.Api.IntegrationTests/Steps/Tenancies/TenancySteps.cs
@@ -9,6 +9,7 @@
 
     using KnightFrank.Antares.Api.IntegrationTests.Extensions;
     using KnightFrank.Antares.Api.IntegrationTests.Fixtures;
+    using KnightFrank.Antares.Dal.Model.Contacts;
     using KnightFrank.Antares.Dal.Model.Property;
     using KnightFrank.Antares.Dal.Model.Property.Activities;
     using KnightFrank.Antares.Dal.Model.Tenancy;
@@ -26,6 +27,7 @@
         private const string ApiUrl = "/api/tenancies";
         private readonly BaseTestClassFixture fixture;
         private readonly ScenarioContext scenarioContext;
+        private readonly DateTime date = DateTime.UtcNow;
 
         public TenancySteps(BaseTestClassFixture fixture, ScenarioContext scenarioContext)
         {
@@ -52,24 +54,48 @@
                 Term = new UpdateTenancyTerm
                 {
                     AgreedRent = 200,
-                    StartDate = new DateTime(2016, 2, 2),
-                    EndDate = new DateTime(2017, 2, 2)
+                    StartDate = this.date.AddDays(1),
+                    EndDate = this.date.AddYears(1)
                 }
             };
 
             this.PostTenancy(tenancyCommand);
         }
 
+        [When(@"User creates tenancy with invalid (.*) using api")]
+        public void WhenUserCreatesTenancyWithInvalidDataUsingApi(string data)
+        {
+            Guid requirementId = data.Equals("requirement") ? Guid.NewGuid() : this.scenarioContext.Get<Requirement>("Requirement").Id;
+            Guid activityId = data.Equals("activity") ? Guid.NewGuid() : this.scenarioContext.Get<Activity>("Activity").Id;
+
+            var tenancyCommand = new CreateTenancyCommand
+            {
+                ActivityId = activityId,
+                RequirementId = requirementId,
+                LandlordContacts = new List<Guid>(),
+                TenantContacts = new List<Guid>(),
+                Term = new UpdateTenancyTerm
+                {
+                    AgreedRent = 200,
+                    StartDate = this.date.AddDays(1),
+                    EndDate = this.date.AddYears(1)
+                }
+            };
+
+            this.PostTenancy(tenancyCommand);
+        }
+
+
         [Then(@"Tenancy should be the same as added")]
         public void ThenTenancyShouldBeTheSameAsAdded()
         {
-            var currectTenancy = JsonConvert.DeserializeObject<Tenancy>(this.scenarioContext.GetResponseContent());
+            var currentTenancy = JsonConvert.DeserializeObject<Tenancy>(this.scenarioContext.GetResponseContent());
             Tenancy expectedTenancy =
-                this.fixture.DataContext.Tenancies.Single(req => req.Id.Equals(currectTenancy.Id));
+                this.fixture.DataContext.Tenancies.Single(req => req.Id.Equals(currentTenancy.Id));
 
-            expectedTenancy.ActivityId.Should().Be(currectTenancy.ActivityId);
-            expectedTenancy.RequirementId.Should().Be(currectTenancy.RequirementId);
-            expectedTenancy.Terms.First().ShouldBeEquivalentTo(currectTenancy.Terms.First());
+            expectedTenancy.ActivityId.Should().Be(currentTenancy.ActivityId);
+            expectedTenancy.RequirementId.Should().Be(currentTenancy.RequirementId);
+            expectedTenancy.Terms.First().ShouldBeEquivalentTo(currentTenancy.Terms.First());
         }
 
         private void PostTenancy(CreateTenancyCommand command)
@@ -77,5 +103,52 @@
             HttpResponseMessage response = this.fixture.SendPostRequest(ApiUrl, command);
             this.scenarioContext.SetHttpResponseMessage(response);
         }
+
+        private void UpdateTenancy(UpdateTenancyCommand command)
+        {
+            HttpResponseMessage response = this.fixture.SendPutRequest(ApiUrl, command);
+            this.scenarioContext.SetHttpResponseMessage(response);
+        }
+
+        [Given(@"tenancy exists in database")]
+        public void GivenTenancyExistsInDatabase()
+        {
+            var requirementId = this.scenarioContext.Get<Requirement>("Requirement").Id;
+            var activityId = this.scenarioContext.Get<Activity>("Activity").Id;
+            var tenancy = new Tenancy
+            {
+                RequirementId = requirementId,
+                ActivityId = activityId,
+                TenancyTypeId = this.fixture.DataContext.TenancyTypes.Single(t => t.EnumCode == nameof(Domain.Common.Enums.TenancyType.ResidentialLetting)).Id
+            };
+
+            this.fixture.DataContext.Tenancies.Add(tenancy);
+            this.fixture.DataContext.SaveChanges();
+
+            this.fixture.DataContext.Terms.Add(new TenancyTerm { AgreedRent = 10, EndDate = this.date.AddYears(1) , StartDate = this.date.AddDays(1) , TenancyId = tenancy.Id });
+            this.fixture.DataContext.SaveChanges();
+
+            this.scenarioContext.Set(tenancy, "Tenancy");
+        }
+
+        [When(@"User updates tenancy with terms")]
+        public void WhenUserUpdatesTenancyWithTerms()
+        {
+            Guid tenancyId = this.scenarioContext.Get<Tenancy>("Tenancy").Id;
+
+            var upadteTenancyCommand = new UpdateTenancyCommand
+            {
+                TenancyId = tenancyId,
+                Term = new UpdateTenancyTerm
+                {
+                    AgreedRent = 200,
+                    StartDate = this.date.AddDays(1),
+                    EndDate = this.date.AddYears(1)
+                }
+            };
+
+            this.UpdateTenancy(upadteTenancyCommand);
+        }
+
     }
 }
--- src/Tests/KnightFrank.Antares.Api.IntegrationTests/Tests/Tenancies/Tenancy.feature
+++ src/Tests/KnightFrank.Antares.Api.IntegrationTests/Tests/Tenancies/Tenancy.feature
@@ -6,12 +6,49 @@ Scenario: Create tenancy
 		| PropertyType | Division    |
 		| House        | Residential |
 		And Activity exists in database
+			| ActivityStatus | ActivityType        |
+			| PreAppraisal   | Open Market Letting |
+		And Contacts exists in database
+		| Title  | FirstName | LastName |
+		| Mister | Tomasz    | Bien     |
+		And Requirement of type ResidentialLetting exists in database
+	When User creates tenancy for latest requirement and activity
+	Then User should get OK http status code
+		And Tenancy should be the same as added
+
+@Tenancies
+Scenario Outline: Create tenancy with invalid data
+	Given Property exists in database
+		| PropertyType | Division    |
+		| House        | Residential |
+		And Activity exists in database
+			| ActivityStatus | ActivityType  |
+			| PreAppraisal   | Open Market Letting |
+		And Contacts exists in database
+		| Title  | FirstName | LastName |
+		| Mister | Tomasz    | Bien     |
+		And Requirement of type ResidentialLetting exists in database
+	When User creates tenancy with invalid <data> using api
+	Then User should get BadRequest http status code
+
+	Examples: 
+	| data        |
+	| requirement |
+	| activity    |
+
+@Tenancies
+Scenario: Update tenancy
+	Given Property exists in database
+		| PropertyType | Division    |
+		| House        | Residential |
+		And Activity exists in database
 			| ActivityStatus | ActivityType  |
 			| PreAppraisal   | Freehold Sale |
 		And Contacts exists in database
 		| Title  | FirstName | LastName |
 		| Mister | Tomasz    | Bien     |
 		And Requirement of type ResidentialLetting exists in database
-	When User creates tenancy for latest requirement and activity
+		And tenancy exists in database
+	When User updates tenancy with terms
 	Then User should get OK http status code
 		And Tenancy should be the same as added
